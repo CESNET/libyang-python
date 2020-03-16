CESNET/libyang-python

commit 49bc67856360305e0fd393f7f7f39875032e96f6

Add cffi binding
igned-off-by: Robin Jarry <robin@jarry.cc>

libyang/schema.py
@@ -1,0 +1,366 @@
+# Copyright (c) 2018 Robin Jarry
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+from _libyang import ffi
+from _libyang import lib
+
+from .util import c2str
+from .util import str2c
+
+
+#------------------------------------------------------------------------------
+class Module:
+
+    def __init__(self, context, module_p):
+        self.context = context
+        self._module = module_p
+
+    def name(self):
+        return c2str(self._module.name)
+
+    def prefix(self):
+        return c2str(self._module.prefix)
+
+    def dsc(self):
+        return c2str(self._module.dsc)
+
+    def find_path(self, path):
+        node_set = ffi.gc(lib.ly_ctx_find_path(self._ctx, str2c(path)),
+                          lib.ly_set_free)
+        if not node_set:
+            raise self.error('cannot find path')
+
+        for i in range(node_set.number):
+            yield Node.new(self, node_set.set.s[i])
+
+    def __iter__(self):
+        return self.children()
+
+    def children(self, types=None):
+        return iter_children(self.context, module=self._module, types=types)
+
+    def __str__(self):
+        return self.dump_str()
+
+    def dump_str(self, fmt=lib.LYS_OUT_TREE, path=None):
+        buf = ffi.new('char **')
+        ret = lib.lys_print_mem(buf, self._module, fmt, str2c(path), 0, 0)
+        if ret != 0:
+            raise self.context.error('cannot print module')
+        return c2str(buf[0])
+
+    def dump_file(self, fileobj, fmt=lib.LYS_OUT_TREE, path=None):
+        ret = lib.lys_print_fd(
+            fileobj.fileno(), self._module, fmt, str2c(path), 0, 0)
+        if ret != 0:
+            raise self.context.error('cannot print module')
+
+
+#------------------------------------------------------------------------------
+class Extension:
+
+    def __init__(self, context, ext_p):
+        self.context = context
+        self._ext = ext_p
+        self._def = ext_p['def']
+
+    def name(self):
+        return c2str(self._def.name)
+
+    def argument(self):
+        return c2str(self._ext.arg_value)
+
+    def module(self):
+        module_p = lib.lys_main_module(self._def.module)
+        if not module_p:
+            raise self.context.error('cannot get module')
+        return Module(self.context, module_p)
+
+
+#------------------------------------------------------------------------------
+class Type:
+
+    def __init__(self, context, type_p):
+        self.context = context
+        self._type = type_p
+
+    def get_builtins(self):
+        if self._type.base == lib.LY_TYPE_DER:
+            yield from self.parent().get_builtins()
+        elif self._type.base == lib.LY_TYPE_LEAFREF:
+            lref = self._type.info.lref
+            yield from Type(self.context, ffi.addressof(lref.target.type)).get_builtins()
+        elif self._type.base == lib.LY_TYPE_UNION:
+            t = lib.lys_getnext_union_type(ffi.NULL, self._type)
+            while t:
+                yield from Type(self.context, t).get_builtins()
+                t = lib.lys_getnext_union_type(t, self._type)
+        else:  # builtin type
+            yield self
+
+    def name(self):
+        return c2str(self._type.der.name)
+
+    def dsc(self):
+        return c2str(self._type.der.dsc)
+
+    def base(self):
+        return self._type.base
+
+    BASENAMES = {
+        lib.LY_TYPE_DER: 'derived',
+        lib.LY_TYPE_BINARY: 'binary',
+        lib.LY_TYPE_BITS: 'bits',
+        lib.LY_TYPE_BOOL: 'boolean',
+        lib.LY_TYPE_DEC64: 'decimal64',
+        lib.LY_TYPE_EMPTY: 'empty',
+        lib.LY_TYPE_ENUM: 'enumeration',
+        lib.LY_TYPE_IDENT: 'ident',
+        lib.LY_TYPE_INST: 'instance-id',
+        lib.LY_TYPE_LEAFREF: 'leafref',
+        lib.LY_TYPE_STRING: 'string',
+        lib.LY_TYPE_UNION: 'union',
+        lib.LY_TYPE_INT8: 'int8',
+        lib.LY_TYPE_UINT8: 'uint8',
+        lib.LY_TYPE_INT16: 'int16',
+        lib.LY_TYPE_UINT16: 'uint16',
+        lib.LY_TYPE_INT32: 'int32',
+        lib.LY_TYPE_UINT32: 'uint32',
+        lib.LY_TYPE_INT64: 'int64',
+        lib.LY_TYPE_UINT64: 'uint64',
+    }
+
+    def basename(self):
+        return self.BASENAMES.get(self._type.base, 'unknown')
+
+    def parent(self):
+        return Type(self.context, ffi.addressof(self._type.parent.type))
+
+    def module(self):
+        module_p = lib.lys_main_module(self._type.der.module)
+        if not module_p:
+            raise self.context.error('cannot get module')
+        return Module(self.context, module_p)
+
+    def extensions(self):
+        for i in range(self._type.ext_size):
+            yield Extension(self.context, self._type.ext[i])
+        for i in range(self._type.der.ext_size):
+            yield Extension(self.context, self._type.der.ext[i])
+
+    def get_extension(self, name, prefix=None, arg_value=None):
+        ext = lib.lys_find_ext(
+            self._type.ext, self._type.ext_size,
+            str2c(name), str2c(prefix), str2c(arg_value))
+        if not ext and self._type.parent:
+            ext = lib.lys_find_ext(
+                self._type.parent.ext, self._type.parent.ext_size,
+                str2c(name), str2c(prefix), str2c(arg_value))
+        if ext:
+            return Extension(self.context, ext)
+        return None
+
+
+#------------------------------------------------------------------------------
+class Node:
+
+    def __init__(self, context, node_p):
+        self.context = context
+        self._node = node_p
+
+    def keyword(self):
+        return '???'
+
+    def name(self):
+        return c2str(self._node.name)
+
+    def dsc(self):
+        return c2str(self._node.dsc)
+
+    def module(self):
+        module_p = lib.lys_node_module(self._node)
+        if not module_p:
+            raise self.context.error('cannot get module')
+        return Module(self.context, module_p)
+
+    def schema_path(self):
+        return c2str(ffi.gc(lib.lys_path(self._node, 0), lib.free))
+
+    def data_path(self):
+        return c2str(ffi.gc(lib.lys_data_path_pattern(self._node), lib.free))
+
+    def extensions(self):
+        for i in range(self._node.ext_size):
+            yield Extension(self.context, self._node.ext[i])
+
+    def get_extension(self, name, prefix=None, arg_value=None):
+        ext = lib.lys_find_ext(
+            self._node.ext, self._node.ext_size,
+            str2c(name), str2c(prefix), str2c(arg_value))
+        if ext:
+            return Extension(self.context, ext)
+        return None
+
+    @staticmethod
+    def new(context, node_p):
+        nodecls = NODETYPE_CLASS.get(node_p.nodetype, Node)
+        return nodecls(context, node_p)
+
+
+#------------------------------------------------------------------------------
+class Leaf(Node):
+
+    def __init__(self, context, node_p):
+        super().__init__(context, node_p)
+        self._leaf = ffi.cast('struct lys_node_leaf *', node_p)
+
+    def keyword(self):
+        return 'leaf'
+
+    def default(self):
+        return c2str(self._leaf.dflt)
+
+    def units(self):
+        return c2str(self._leaf.units)
+
+    def type(self):
+        return Type(self.context, ffi.addressof(self._leaf.type))
+
+    def is_key(self):
+        if lib.lys_is_key(self._leaf, ffi.NULL):
+            return True
+        return False
+
+
+#------------------------------------------------------------------------------
+class LeafList(Node):
+
+    def __init__(self, context, node_p):
+        super().__init__(context, node_p)
+        self._leaflist = ffi.cast('struct lys_node_leaflist *', node_p)
+
+    def keyword(self):
+        return 'leaf-list'
+
+    def units(self):
+        return c2str(self._leaf.units)
+
+    def type(self):
+        return Type(self.context, ffi.addressof(self._leaflist.type))
+
+    def defaults(self):
+        for i in range(self._leaflist.dflt_size):
+            yield c2str(self._leaflist.dflt[i])
+
+
+#------------------------------------------------------------------------------
+class Container(Node):
+
+    def __init__(self, context, node_p):
+        super().__init__(context, node_p)
+        self._container = ffi.cast('struct lys_node_container *', node_p)
+
+    def keyword(self):
+        return 'container'
+
+    def presence(self):
+        return c2str(self._container.presence)
+
+    def __iter__(self):
+        return self.children()
+
+    def children(self, types=None):
+        return iter_children(self.context, parent=self._node, types=types)
+
+
+#------------------------------------------------------------------------------
+class List(Node):
+
+    def __init__(self, context, node_p):
+        super().__init__(context, node_p)
+        self._list = ffi.cast('struct lys_node_list *', node_p)
+
+    def keyword(self):
+        return 'list'
+
+    def __iter__(self):
+        return self.children()
+
+    def children(self, skip_keys=False, types=None):
+        return iter_children(
+            self.context, parent=self._node, skip_keys=skip_keys, types=types)
+
+    def keys(self):
+        for i in range(self._list.keys_size):
+            node = ffi.cast('struct lys_node *', self._list.keys[i])
+            yield Leaf(self.context, node)
+
+
+#------------------------------------------------------------------------------
+class Rpc(Node):
+
+    def __init__(self, context, node_p):
+        super().__init__(context, node_p)
+        self._rpc = ffi.cast('struct lys_node_rpc_action *', node_p)
+
+    def keyword(self):
+        return 'rpc'
+
+    def __iter__(self):
+        return self.children()
+
+    def children(self, types=None):
+        return iter_children(self.context, parent=self._node, types=types)
+
+
+#------------------------------------------------------------------------------
+def iter_children(context, parent=ffi.NULL, module=ffi.NULL, *,
+                  skip_keys=False, types=None):
+    if types is None:
+        types = (lib.LYS_CONTAINER, lib.LYS_LIST, lib.LYS_RPC,
+                 lib.LYS_LEAF, lib.LYS_LEAFLIST)
+
+    def _skip(node):
+        if node.nodetype not in types:
+            return True
+        if not skip_keys:
+            return False
+        if node.nodetype != lib.LYS_LEAF:
+            return False
+        leaf = ffi.cast('struct lys_node_leaf *', node)
+        if lib.lys_is_key(leaf, ffi.NULL):
+            return True
+        return False
+
+    child = lib.lys_getnext(ffi.NULL, parent, module, 0)
+    while child:
+        if not _skip(child):
+            yield Node.new(context, child)
+        child = lib.lys_getnext(child, parent, module, 0)
+
+
+#------------------------------------------------------------------------------
+NODETYPE_CLASS = {
+    lib.LYS_CONTAINER: Container,
+    lib.LYS_LEAF: Leaf,
+    lib.LYS_LEAFLIST: LeafList,
+    lib.LYS_LIST: List,
+    lib.LYS_RPC: Rpc,
+}

MANIFEST.in
@@ -1,0 +1,14 @@
+graft cffi
+graft clib
+prune clib/tests
+prune clib/swig
+prune clib/tools/lint/examples
+graft libyang
+include setup.py
+include setup.cfg
+include LICENSE
+include README.md
+global-exclude *.py[co]
+global-exclude *.so
+global-exclude *.o
+global-exclude *.a

cffi/build.py
@@ -1,0 +1,34 @@
+# Copyright (c) 2018 Robin Jarry
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+import os
+
+import cffi
+
+
+HERE = os.path.dirname(__file__)
+BUILDER = cffi.FFI()
+with open(os.path.join(HERE, 'cdefs.h')) as f:
+    BUILDER.cdef(f.read())
+with open(os.path.join(HERE, 'source.c')) as f:
+    BUILDER.set_source('_libyang', f.read(), libraries=['yang'])
+
+if __name__ == '__main__':
+    BUILDER.compile()

cffi/cdefs.h
@@ -1,0 +1,266 @@
+/*
+ * Copyright (c) 2018 Robin Jarry
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+struct ly_ctx;
+
+#define LY_CTX_ALLIMPLEMENTED ...
+#define LY_CTX_TRUSTED ...
+#define LY_CTX_NOYANGLIBRARY ...
+#define LY_CTX_DISABLE_SEARCHDIRS ...
+#define LY_CTX_DISABLE_SEARCHDIR_CWD ...
+#define LY_CTX_PREFER_SEARCHDIRS ...
+
+struct ly_ctx *ly_ctx_new(const char *, int);
+int ly_ctx_set_searchdir(struct ly_ctx *, const char *);
+void ly_ctx_destroy(struct ly_ctx *, void *);
+
+typedef enum {
+	LY_LLERR,
+	LY_LLWRN,
+	LY_LLVRB,
+	LY_LLDBG,
+	...
+} LY_LOG_LEVEL;
+
+LY_LOG_LEVEL ly_verb(LY_LOG_LEVEL);
+extern "Python" void logging_callback(LY_LOG_LEVEL, const char *, const char *);
+void ly_set_log_clb(void (*)(LY_LOG_LEVEL, const char *, const char *), int);
+const char *ly_errmsg(const struct ly_ctx *);
+const char *ly_errpath(const struct ly_ctx *);
+
+struct lys_module {
+	const char *name;
+	const char *prefix;
+	const char *dsc;
+	...;
+};
+
+struct lys_ext {
+	const char *name;
+	struct lys_module *module;
+	...;
+};
+
+struct lys_ext_instance {
+	struct lys_ext *def;
+	const char *arg_value;
+	...;
+};
+
+typedef enum {
+	LY_TYPE_DER,
+	LY_TYPE_BINARY,
+	LY_TYPE_BITS,
+	LY_TYPE_BOOL,
+	LY_TYPE_DEC64,
+	LY_TYPE_EMPTY,
+	LY_TYPE_ENUM,
+	LY_TYPE_IDENT,
+	LY_TYPE_INST,
+	LY_TYPE_LEAFREF,
+	LY_TYPE_STRING,
+	LY_TYPE_UNION,
+	LY_TYPE_INT8,
+	LY_TYPE_UINT8,
+	LY_TYPE_INT16,
+	LY_TYPE_UINT16,
+	LY_TYPE_INT32,
+	LY_TYPE_UINT32,
+	LY_TYPE_INT64,
+	LY_TYPE_UINT64,
+	LY_TYPE_UNKNOWN,
+	...
+} LY_DATA_TYPE;
+
+struct lys_type_bit {
+	const char *name;
+	const char *dsc;
+	uint32_t pos;
+	...;
+};
+
+struct lys_type_info_bits {
+	struct lys_type_bit *bit;
+	unsigned int count;
+};
+
+struct lys_type_enum {
+	const char *name;
+	const char *dsc;
+	...;
+};
+
+struct lys_type_info_enums {
+	struct lys_type_enum *enm;
+	unsigned int count;
+};
+
+struct lys_type_info_lref {
+	const char *path;
+	struct lys_node_leaf* target;
+	int8_t req;
+};
+
+struct lys_type_info_union {
+	struct lys_type *types;
+	unsigned int count;
+	int has_ptr_type;
+};
+
+union lys_type_info {
+	struct lys_type_info_bits bits;
+	struct lys_type_info_enums enums;
+	struct lys_type_info_lref lref;
+	struct lys_type_info_union uni;
+	...;
+};
+
+struct lys_type {
+	LY_DATA_TYPE base;
+	uint8_t value_flags;
+	uint8_t ext_size;
+	struct lys_ext_instance **ext;
+	struct lys_tpdf *der;
+	struct lys_tpdf *parent;
+	union lys_type_info info;
+	...;
+};
+
+struct lys_tpdf {
+	const char *name;
+	const char *dsc;
+	uint8_t ext_size;
+	struct lys_ext_instance **ext;
+	const char *units;
+	struct lys_type type;
+	const char *dflt;
+	...;
+};
+
+typedef enum lys_nodetype {
+	LYS_UNKNOWN,
+	LYS_CONTAINER,
+	LYS_CHOICE,
+	LYS_LEAF,
+	LYS_LEAFLIST,
+	LYS_LIST,
+	LYS_ANYXML,
+	LYS_CASE,
+	LYS_NOTIF,
+	LYS_RPC,
+	LYS_INPUT,
+	LYS_OUTPUT,
+	LYS_GROUPING,
+	LYS_USES,
+	LYS_AUGMENT,
+	LYS_ACTION,
+	LYS_ANYDATA,
+	LYS_EXT,
+	...
+} LYS_NODE;
+
+
+struct lys_node {
+	const char *name;
+	const char *dsc;
+	uint8_t ext_size;
+	struct lys_ext_instance **ext;
+	LYS_NODE nodetype;
+	...;
+};
+
+struct lys_node_container {
+	const char *presence;
+	...;
+};
+
+struct lys_node_leaf {
+	struct lys_type type;
+	const char *units;
+	const char *dflt;
+	...;
+};
+
+struct lys_node_leaflist {
+	struct lys_type type;
+	const char *units;
+	const char **dflt;
+	uint32_t min;
+	uint32_t max;
+	...;
+};
+
+struct lys_node_list {
+	uint8_t keys_size;
+	struct lys_node_leaf **keys;
+	uint32_t min;
+	uint32_t max;
+	...;
+};
+struct lys_node_rpc_action {
+	...;
+};
+
+union ly_set_set {
+	struct lys_node **s;
+	...;
+};
+
+struct ly_set {
+	unsigned int size;
+	unsigned int number;
+	union ly_set_set set;
+};
+
+const struct lys_module *ly_ctx_load_module(struct ly_ctx *, const char *, const char *);
+const struct lys_module *ly_ctx_get_module_iter(const struct ly_ctx *, uint32_t *);
+const struct lys_module *ly_ctx_get_module(const struct ly_ctx *, const char *, const char *, int);
+struct ly_set *ly_ctx_find_path(struct ly_ctx *, const char *);
+void ly_set_free(struct ly_set *set);
+const struct lys_node_list *lys_is_key(const struct lys_node_leaf *, uint8_t *);
+const struct lys_node *lys_getnext(const struct lys_node *, const struct lys_node *, const struct lys_module *, int);
+const struct lys_type *lys_getnext_union_type(const struct lys_type *last, const struct lys_type *type);
+char *lys_data_path(const struct lys_node *);
+char *lys_path(const struct lys_node *, int);
+struct lys_module *lys_node_module(const struct lys_node *);
+struct lys_module *lys_main_module(const struct lys_module *);
+struct lys_node *lys_parent(const struct lys_node *);
+
+typedef enum {
+	LYS_OUT_UNKNOWN,
+	LYS_OUT_YANG,
+	LYS_OUT_YIN,
+	LYS_OUT_TREE,
+	LYS_OUT_INFO,
+	LYS_OUT_JSON,
+	...
+} LYS_OUTFORMAT;
+
+int lys_print_mem(char **, const struct lys_module *, LYS_OUTFORMAT, const char *, int, int);
+int lys_print_fd(int, const struct lys_module *, LYS_OUTFORMAT, const char *, int, int);
+
+/* extra functions */
+const struct lys_ext_instance *lys_find_ext(
+	const struct lys_ext_instance **, uint8_t,
+	const char *, const char *, const char *);
+char *lys_data_path_pattern(const struct lys_node *);
+void free(void *);

cffi/source.c
@@ -1,0 +1,120 @@
+/*
+ * Copyright (c) 2018 Robin Jarry
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+#include <libyang/libyang.h>
+
+static const struct lys_ext_instance *
+lys_find_ext(const struct lys_ext_instance **ext, uint8_t ext_size,
+	const char *name, const char *prefix, const char *arg_value)
+{
+	const struct lys_ext_instance *inst;
+	const struct lys_module *module;
+	const struct lys_ext *def;
+	uint8_t i;
+
+	if (!ext)
+		goto notfound;
+
+	for (i = 0; i < ext_size; i++) {
+		inst = ext[i];
+		def = inst->def;
+		if (name && strcmp(def->name, name) != 0)
+			continue;
+		if (prefix) {
+			module = lys_main_module(def->module);
+			if (!module)
+				goto notfound;
+			if (strcmp(module->name, prefix) != 0)
+				continue;
+		}
+		if (arg_value && inst->arg_value) {
+			if (strcmp(arg_value, inst->arg_value) != 0)
+				continue;
+		}
+		return inst;
+	}
+
+notfound:
+	return NULL;
+}
+
+static char *lys_data_path_pattern(const struct lys_node *node)
+{
+	const struct lys_module *prev_mod, *mod;
+	char *xpath = NULL, *keys = NULL;
+	struct ly_set *set = NULL;;
+	size_t x;
+
+	if (!node)
+		goto cleanup;
+
+	set = ly_set_new();
+	if (!set)
+		goto cleanup;
+
+	while (node) {
+		ly_set_add(set, (void *)node, 0);
+		do {
+			node = lys_parent(node);
+		} while (node && !(node->nodetype & (
+			LYS_CONTAINER | LYS_LIST | LYS_RPC)));
+	}
+
+	xpath = malloc(2048);
+	if (!xpath)
+		goto cleanup;
+	keys = malloc(512);
+	if (!keys)
+		goto cleanup;
+
+	x = 0;
+	xpath[0] = '\0';
+
+	prev_mod = NULL;
+	for (int i = set->number - 1; i > -1; --i) {
+		size_t k = 0;
+		keys[0] = '\0';
+		node = set->set.s[i];
+		if (node->nodetype == LYS_LIST) {
+			const struct lys_node_list *list;
+			list = (const struct lys_node_list *)node;
+			for (uint8_t j = 0; j < list->keys_size; j++) {
+				k += sprintf(keys + k, "[%s='%%s']",
+					list->keys[j]->name);
+			}
+		}
+
+		mod = lys_node_module(node);
+		if (mod && mod != prev_mod) {
+			prev_mod = mod;
+			x += sprintf(xpath + x, "/%s:%s%s",
+				mod->name, node->name, keys);
+		} else {
+			x += sprintf(xpath + x, "/%s%s", node->name, keys);
+		}
+	}
+
+cleanup:
+	ly_set_free(set);
+	free(keys);
+	return xpath;
+}

libyang/__init__.py
@@ -1,0 +1,129 @@
+# Copyright (c) 2018 Robin Jarry
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+import os
+import logging
+
+from _libyang import ffi
+from _libyang import lib
+
+from .schema import Module
+from .util import LibyangError
+from .util import str2c
+from .util import c2str
+from libyang.schema import Node
+
+
+#------------------------------------------------------------------------------
+class Context(object):
+
+    def __init__(self, search_dir, options=lib.LY_CTX_DISABLE_SEARCHDIR_CWD):
+        if search_dir and os.path.isdir(search_dir):
+            search_dir = str2c(search_dir)
+        else:
+            search_dir = ffi.NULL
+        self._ctx = ffi.gc(lib.ly_ctx_new(search_dir, options),
+                           lambda c: lib.ly_ctx_destroy(c, ffi.NULL))
+        if not self._ctx:
+            raise self.error('cannot create context')
+
+        for path in os.environ.get('YANG_MODPATH', '').split(os.pathsep):
+            if not os.path.isdir(path):
+                continue
+            if lib.ly_ctx_set_searchdir(self._ctx, str2c(path)) != 0:
+                raise self.error('cannot set search dir')
+
+    def error(self, msg, *args):
+        fmt = msg
+        ly_path = lib.ly_errpath(self._ctx)
+        if ly_path:
+            args += (c2str(ly_path),)
+            fmt += ': %s'
+        ly_msg = lib.ly_errmsg(self._ctx)
+        if ly_msg:
+            args += (c2str(ly_msg),)
+            fmt += ': %s'
+        return LibyangError(fmt % args)
+
+    def load_module(self, name):
+        mod = lib.ly_ctx_load_module(self._ctx, str2c(name), ffi.NULL)
+        if not mod:
+            raise self.error('cannot load module')
+
+        return Module(self, mod)
+
+    def get_module(self, name):
+        mod = lib.ly_ctx_get_module(self._ctx, str2c(name), ffi.NULL, False)
+        if not mod:
+            raise self.error('cannot get module')
+
+        return Module(self, mod)
+
+    def find_path(self, path):
+        node_set = ffi.gc(lib.ly_ctx_find_path(self._ctx, str2c(path)),
+                          lib.ly_set_free)
+        if not node_set:
+            raise self.error('cannot find path')
+
+        for i in range(node_set.number):
+            yield Node.new(self, node_set.set.s[i])
+
+    def __iter__(self):
+        """
+        Return an iterator that yields all implemented modules from the context
+        """
+        idx = ffi.new('uint32_t *')
+        mod = lib.ly_ctx_get_module_iter(self._ctx, idx)
+        while mod:
+            yield Module(self, mod)
+            mod = lib.ly_ctx_get_module_iter(self._ctx, idx)
+
+
+#------------------------------------------------------------------------------
+LOG_LEVELS = {
+    lib.LY_LLERR: logging.ERROR,
+    lib.LY_LLWRN: logging.WARNING,
+    lib.LY_LLVRB: logging.INFO,
+    lib.LY_LLDBG: logging.DEBUG,
+}
+
+
+@ffi.def_extern(name='logging_callback')
+def libyang_c_logging_callback(level, msg, path):
+    args = [c2str(msg)]
+    if path:
+        fmt = '%s: %s'
+        args.append(c2str(path))
+    else:
+        fmt = '%s'
+    LOG.log(LOG_LEVELS.get(level, logging.NOTSET), fmt, *args)
+
+
+def set_log_level(level):
+    for ly_lvl, py_lvl in LOG_LEVELS.items():
+        if py_lvl == level:
+            lib.ly_verb(ly_lvl)
+            return
+
+
+set_log_level(logging.WARNING)
+lib.ly_set_log_clb(lib.logging_callback, True)
+LOG = logging.getLogger(__name__)
+LOG.addHandler(logging.NullHandler())

commit a1d50b0383a1e7e05f4239615515fea927fc54c5

schema: fix memory leak in Module.dump_str
Must call lib.free on the allocated buffer.

Fixes: 49bc67856360 ("Add cffi binding")
Signed-off-by: Robin Jarry <robin@jarry.cc>

libyang/schema.py
@@ -77,7 +77,10 @@ class Module(object):
         ret = lib.lys_print_mem(buf, self._module, fmt, str2c(path), 0, 0)
         if ret != 0:
             raise self.context.error('cannot print module')
-        return c2str(buf[0])
+        s = c2str(buf[0])
+        if s is not None:
+            lib.free(buf[0])
+        return s
 
     def dump_file(self, fileobj, fmt=lib.LYS_OUT_TREE, path=None):
         ret = lib.lys_print_fd(
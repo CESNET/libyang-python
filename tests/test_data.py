CESNET/libyang-python

commit 49fd8ea4dc0d89659ef02c4964de19a7b1625ba1

Add data tree support
Add support for parsing various formats to DNode objects, constructing
DNode objects from scratch and serializing them back to various formats.

Add basic unit tests.

Signed-off-by: Robin Jarry <robin.jarry@6wind.com>

libyang/__init__.py
@@ -1,4 +1,5 @@
 # Copyright (c) 2018-2019 Robin Jarry
+# Copyright (c) 2020 6WIND S.A.
 # SPDX-License-Identifier: MIT
 
 import os  # isort:skip
@@ -18,6 +19,8 @@ import logging
 from _libyang import ffi
 from _libyang import lib
 
+from .data import DNode
+from .data import PathOpt
 from .schema import Module
 from .schema import SNode
 from .util import LibyangError
@@ -103,6 +106,46 @@ class Context(object):
                 yield SNode.new(self, node_set.set.s[i])
         finally:
             lib.ly_set_free(node_set)
+
+    def create_data_path(self, path, parent=None, value=None, flags=0):
+        lib.lypy_set_errno(0)
+        if value is not None and not isinstance(value, str):
+            value = str(value)
+        dnode = lib.lyd_new_path(
+            parent._node if parent else ffi.NULL,
+            self._ctx, str2c(path), str2c(value), 0,
+            PathOpt.UPDATE | PathOpt.NOPARENTRET | flags)
+        if lib.lypy_get_errno() != 0:
+            raise self.error('cannot create data path: %s', path)
+
+        if not dnode and parent:
+            # This can happen when path points to an already created leaf and
+            # its value does not change.
+            # In that case, lookup the existing leaf and return it.
+            node_set = lib.lyd_find_path(parent._node, str2c(path))
+            try:
+                if not node_set or not node_set.number:
+                    raise self.error('cannot find path')
+                dnode = node_set.set.s[0]
+            finally:
+                lib.ly_set_free(node_set)
+
+        if not dnode:
+            raise self.error('cannot find created path')
+
+        return DNode.new(self, dnode)
+
+    def parse_data_str(self, s, fmt, flags=0):
+        dnode = lib.lyd_parse_mem(self._ctx, str2c(s), fmt, flags)
+        if not dnode:
+            raise self.error('failed to parse data tree')
+        return DNode.new(self, dnode)
+
+    def parse_data_file(self, fileobj, fmt, flags=0):
+        dnode = lib.lyd_parse_fd(self._ctx, fileobj.fileno(), fmt, flags)
+        if not dnode:
+            raise self.error('failed to parse data tree')
+        return DNode.new(self, dnode)
 
     def __iter__(self):
         """

tests/test_data.py
@@ -1,0 +1,220 @@
+# Copyright (c) 2020 6WIND S.A.
+# SPDX-License-Identifier: MIT
+
+import json
+import os
+import unittest
+
+from libyang import Context
+from libyang import LibyangError
+from libyang.data import DContainer
+from libyang.data import ParserOpt
+from libyang.data import PrintFmt
+from libyang.data import PrintOpt
+from libyang.data import dict_to_dnode
+from libyang.data import dnode_to_dict
+
+
+YANG_DIR = os.path.join(os.path.dirname(__file__), 'yang')
+
+
+#------------------------------------------------------------------------------
+class DataTest(unittest.TestCase):
+
+    def setUp(self):
+        self.ctx = Context(YANG_DIR)
+        mod = self.ctx.load_module('yolo-system')
+        mod.feature_enable_all()
+
+    def tearDown(self):
+        self.ctx = None
+
+    JSON_CONFIG = '''{
+  "yolo-system:conf": {
+    "hostname": "foo",
+    "speed": 1234,
+    "number": [
+      1000,
+      2000,
+      3000
+    ],
+    "url": [
+      {
+        "proto": "https",
+        "host": "github.com",
+        "path": "/rjarry/libyang-cffi"
+      },
+      {
+        "proto": "http",
+        "host": "foobar.com",
+        "port": 8080,
+        "path": "/index.html"
+      }
+    ]
+  }
+}
+'''
+
+    def test_data_parse_config_json(self):
+        dnode = self.ctx.parse_data_str(
+            self.JSON_CONFIG, PrintFmt.JSON, ParserOpt.CONFIG)
+        self.assertIsInstance(dnode, DContainer)
+        j = dnode.dump_str(PrintFmt.JSON, PrintOpt.PRETTY)
+        self.assertEqual(j, self.JSON_CONFIG)
+        dnode.free()
+
+    JSON_STATE = '''{
+  "yolo-system:state": {
+    "hostname": "foo",
+    "speed": 1234,
+    "number": [
+      1000,
+      2000,
+      3000
+    ],
+    "url": [
+      {
+        "proto": "https",
+        "host": "github.com",
+        "path": "/rjarry/libyang-cffi"
+      },
+      {
+        "proto": "http",
+        "host": "foobar.com",
+        "port": 8080,
+        "path": "/index.html"
+      }
+    ]
+  }
+}
+'''
+
+    def test_data_parse_state_json(self):
+        dnode = self.ctx.parse_data_str(
+            self.JSON_STATE, PrintFmt.JSON,
+            ParserOpt.DATA | ParserOpt.NO_YANGLIB)
+        self.assertIsInstance(dnode, DContainer)
+        j = dnode.dump_str(PrintFmt.JSON, PrintOpt.PRETTY)
+        self.assertEqual(j, self.JSON_STATE)
+        dnode.free()
+
+    XML_CONFIG = '''<conf xmlns="urn:yang:yolo:system">
+  <hostname>foo</hostname>
+  <speed>1234</speed>
+  <number>1000</number>
+  <number>2000</number>
+  <number>3000</number>
+  <url>
+    <proto>https</proto>
+    <host>github.com</host>
+    <path>/rjarry/libyang-cffi</path>
+  </url>
+  <url>
+    <proto>http</proto>
+    <host>foobar.com</host>
+    <port>8080</port>
+    <path>/index.html</path>
+  </url>
+</conf>
+'''
+
+    def test_data_parse_config_xml(self):
+        dnode = self.ctx.parse_data_str(
+            self.XML_CONFIG, PrintFmt.XML, ParserOpt.CONFIG)
+        self.assertIsInstance(dnode, DContainer)
+        xml = dnode.dump_str(PrintFmt.XML, PrintOpt.PRETTY)
+        self.assertEqual(xml, self.XML_CONFIG)
+        dnode.free()
+
+    XML_STATE = '''<state xmlns="urn:yang:yolo:system">
+  <hostname>foo</hostname>
+  <speed>1234</speed>
+  <number>1000</number>
+  <number>2000</number>
+  <number>3000</number>
+  <url>
+    <proto>https</proto>
+    <host>github.com</host>
+    <path>/rjarry/libyang-cffi</path>
+  </url>
+  <url>
+    <proto>http</proto>
+    <host>foobar.com</host>
+    <port>8080</port>
+    <path>/index.html</path>
+  </url>
+</state>
+'''
+
+    def test_data_parse_data_xml(self):
+        dnode = self.ctx.parse_data_str(
+            self.XML_STATE, PrintFmt.XML,
+            ParserOpt.DATA | ParserOpt.NO_YANGLIB)
+        self.assertIsInstance(dnode, DContainer)
+        xml = dnode.dump_str(PrintFmt.XML, PrintOpt.PRETTY)
+        self.assertEqual(xml, self.XML_STATE)
+        dnode.free()
+
+    def test_data_create_paths(self):
+        state = self.ctx.create_data_path('/yolo-system:state')
+        state.create_path('hostname', 'foo')
+        state.create_path('speed', 1234)
+        state.create_path('number', 1000)
+        state.create_path('number', 2000)
+        state.create_path('number', 3000)
+        u = state.create_path('url[proto="https"][host="github.com"]')
+        u.create_path('path', '/rjarry/libyang-cffi')
+        u = state.create_path('url[proto="http"][host="foobar.com"]')
+        u.create_path('port', 8080)
+        u.create_path('path', '/index.html')
+        state.validate(ParserOpt.STRICT)
+        self.assertEqual(state.dump_str(PrintFmt.JSON, PrintOpt.PRETTY), self.JSON_STATE)
+        state.free()
+
+    def test_data_create_invalid_type(self):
+        s = self.ctx.create_data_path('/yolo-system:state')
+        with self.assertRaises(LibyangError):
+            s.create_path('speed', 1234000000000000000000000000)
+        s.free()
+
+    def test_data_create_invalid_regexp(self):
+        s = self.ctx.create_data_path('/yolo-system:state')
+        with self.assertRaises(LibyangError):
+            s.create_path('hostname', 'INVALID.HOST')
+        s.free()
+
+    DICT_CONFIG = {
+        'conf': {
+            'hostname': 'foo',
+            'speed': 1234,
+            'number': [1000, 2000, 3000],
+            'url': [
+                {
+                    'proto': 'https',
+                    'host': 'github.com',
+                    'path': '/rjarry/libyang-cffi',
+                },
+                {
+                    'proto': 'http',
+                    'host': 'foobar.com',
+                    'port': 8080,
+                    'path': '/index.html',
+                },
+            ],
+        },
+    }
+
+    def test_data_to_dict(self):
+        dnode = self.ctx.parse_data_str(
+            self.JSON_CONFIG, PrintFmt.JSON, ParserOpt.CONFIG)
+        self.assertIsInstance(dnode, DContainer)
+        dic = dnode_to_dict(dnode)
+        dnode.free()
+        self.assertEqual(dic, self.DICT_CONFIG)
+
+    def test_data_from_dict(self):
+        schema = self.ctx.get_module('yolo-system')
+        dnode = dict_to_dnode(self.DICT_CONFIG, schema)
+        j = dnode.dump_str(PrintFmt.JSON, PrintOpt.PRETTY)
+        dnode.free()
+        self.assertEqual(json.loads(j), json.loads(self.JSON_CONFIG))

cffi/cdefs.h
@@ -17,6 +17,11 @@ int ly_ctx_set_searchdir(struct ly_ctx *, const char *);
 void ly_ctx_destroy(struct ly_ctx *, void *);
 
 typedef enum {
+	LY_SUCCESS,
+	...
+} LY_ERR;
+
+typedef enum {
 	LY_LLERR,
 	LY_LLWRN,
 	LY_LLVRB,
@@ -349,6 +354,69 @@ typedef enum {
 int lys_print_mem(char **, const struct lys_module *, LYS_OUTFORMAT, const char *, int, int);
 int lys_print_fd(int, const struct lys_module *, LYS_OUTFORMAT, const char *, int, int);
 
+typedef enum {
+	LYD_XML,
+	LYD_JSON,
+	LYD_LYB,
+	...
+} LYD_FORMAT;
+
+typedef enum {
+	...
+} LYD_ANYDATA_VALUETYPE;
+
+#define LYP_FORMAT ...
+
+#define LYD_ANYDATA_CONSTSTRING ...
+
+#define LYD_PATH_OPT_UPDATE ...
+#define LYD_PATH_OPT_NOPARENTRET ...
+#define LYD_PATH_OPT_OUTPUT ...
+
+#define LYD_OPT_DATA ...
+#define LYD_OPT_CONFIG ...
+#define LYD_OPT_STRICT ...
+#define LYD_OPT_TRUSTED ...
+#define LYD_OPT_DATA_NO_YANGLIB ...
+
+typedef union lyd_value_u {
+	int8_t bln;
+	struct lyd_node *leafref;
+	...;
+} lyd_val;
+
+struct lyd_node {
+	struct lys_node *schema;
+	struct lyd_node *next;
+	struct lyd_node *child;
+	struct lyd_node *parent;
+	...;
+};
+
+struct lyd_node_leaf_list {
+	struct lys_node *schema;
+	struct lyd_node *next;
+	struct lyd_node *parent;
+	const char *value_str;
+	lyd_val value;
+	LY_DATA_TYPE value_type;
+	...;
+};
+
+struct ly_set *lyd_find_instance(const struct lyd_node *, const struct lys_node *);
+struct ly_set *lyd_find_path(const struct lyd_node *, const char *);
+struct lyd_node *lyd_new_path(struct lyd_node *, const struct ly_ctx *, const char *, void *, LYD_ANYDATA_VALUETYPE, int);
+char *lyd_path(const struct lyd_node *);
+struct lys_module *lyd_node_module(struct lyd_node *);
+struct lyd_node *lyd_parse_mem(struct ly_ctx *, const char *, LYD_FORMAT, int);
+struct lyd_node *lyd_parse_fd(struct ly_ctx *, int, LYD_FORMAT, int);
+int lyd_print_fd(int, const struct lyd_node *, LYD_FORMAT, int);
+int lyd_print_mem(char **, const struct lyd_node *, LYD_FORMAT, int);
+double lyd_dec64_to_double(const struct lyd_node *);
+void lyd_free(struct lyd_node *);
+void lyd_free_withsiblings(struct lyd_node *);
+int lyd_validate(struct lyd_node **, int, void *);
+
 /* from libc, needed to free allocated strings */
 void free(void *);
 
@@ -358,3 +426,5 @@ const struct lys_ext_instance *lypy_find_ext(
 	const char *, const char *, const char *);
 char *lypy_data_path_pattern(const struct lys_node *);
 char *lypy_node_fullname(const struct lys_node *);
+LY_ERR lypy_get_errno(void);
+void lypy_set_errno(LY_ERR);

cffi/source.c
@@ -116,3 +116,13 @@ static char *lypy_node_fullname(const struct lys_node *node)
 
 	return fullname;
 }
+
+static LY_ERR lypy_get_errno(void)
+{
+	return ly_errno;
+}
+
+static void lypy_set_errno(LY_ERR err)
+{
+	ly_errno = err;
+}

commit 631eb787d529d0e4db03996d08dc0fbe269c5b41

data: fix create_data_path with booleans
When creating a boolean value, it must be converted to string *and* to
lower case. Otherwise, libyang fails with the following error:

  Invalid value "False" in "enabled" element.

Add unit test cases to cover this.

Fixes: 49fd8ea4dc0d ("Add data tree support")
Signed-off-by: Robin Jarry <robin@jarry.cc>

libyang/__init__.py
@@ -109,8 +109,11 @@ class Context(object):
 
     def create_data_path(self, path, parent=None, value=None, flags=0):
         lib.lypy_set_errno(0)
-        if value is not None and not isinstance(value, str):
-            value = str(value)
+        if value is not None:
+            if isinstance(value, bool):
+                value = str(value).lower()
+            elif not isinstance(value, str):
+                value = str(value)
         dnode = lib.lyd_new_path(
             parent._node if parent else ffi.NULL,
             self._ctx, str2c(path), str2c(value), 0,

tests/test_data.py
@@ -42,13 +42,15 @@ class DataTest(unittest.TestCase):
       {
         "proto": "https",
         "host": "github.com",
-        "path": "/rjarry/libyang-cffi"
+        "path": "/rjarry/libyang-cffi",
+        "enabled": false
       },
       {
         "proto": "http",
         "host": "foobar.com",
         "port": 8080,
-        "path": "/index.html"
+        "path": "/index.html",
+        "enabled": true
       }
     ]
   }
@@ -76,13 +78,15 @@ class DataTest(unittest.TestCase):
       {
         "proto": "https",
         "host": "github.com",
-        "path": "/rjarry/libyang-cffi"
+        "path": "/rjarry/libyang-cffi",
+        "enabled": false
       },
       {
         "proto": "http",
         "host": "foobar.com",
         "port": 8080,
-        "path": "/index.html"
+        "path": "/index.html",
+        "enabled": true
       }
     ]
   }
@@ -108,12 +112,14 @@ class DataTest(unittest.TestCase):
     <proto>https</proto>
     <host>github.com</host>
     <path>/rjarry/libyang-cffi</path>
+    <enabled>false</enabled>
   </url>
   <url>
     <proto>http</proto>
     <host>foobar.com</host>
     <port>8080</port>
     <path>/index.html</path>
+    <enabled>true</enabled>
   </url>
 </conf>
 '''
@@ -136,12 +142,14 @@ class DataTest(unittest.TestCase):
     <proto>https</proto>
     <host>github.com</host>
     <path>/rjarry/libyang-cffi</path>
+    <enabled>false</enabled>
   </url>
   <url>
     <proto>http</proto>
     <host>foobar.com</host>
     <port>8080</port>
     <path>/index.html</path>
+    <enabled>true</enabled>
   </url>
 </state>
 '''
@@ -164,9 +172,11 @@ class DataTest(unittest.TestCase):
         state.create_path('number', 3000)
         u = state.create_path('url[proto="https"][host="github.com"]')
         u.create_path('path', '/rjarry/libyang-cffi')
+        u.create_path('enabled', False)
         u = state.create_path('url[proto="http"][host="foobar.com"]')
         u.create_path('port', 8080)
         u.create_path('path', '/index.html')
+        u.create_path('enabled', True)
         state.validate(ParserOpt.STRICT)
         self.assertEqual(state.dump_str(PrintFmt.JSON, PrintOpt.PRETTY), self.JSON_STATE)
         state.free()
@@ -193,12 +203,14 @@ class DataTest(unittest.TestCase):
                     'proto': 'https',
                     'host': 'github.com',
                     'path': '/rjarry/libyang-cffi',
+                    'enabled': False,
                 },
                 {
                     'proto': 'http',
                     'host': 'foobar.com',
                     'port': 8080,
                     'path': '/index.html',
+                    'enabled': True,
                 },
             ],
         },

tests/test_diff.py
@@ -9,6 +9,8 @@ from libyang.diff import BaseTypeAdded
 from libyang.diff import BaseTypeRemoved
 from libyang.diff import NodeTypeAdded
 from libyang.diff import NodeTypeRemoved
+from libyang.diff import SNodeAdded
+from libyang.diff import SNodeRemoved
 from libyang.diff import StatusAdded
 from libyang.diff import StatusRemoved
 from libyang.diff import schema_diff
@@ -30,6 +32,8 @@ class DiffTest(unittest.TestCase):
         (NodeTypeAdded, '/yolo-system:state/yolo-system:number'),
         (NodeTypeRemoved, '/yolo-system:conf/yolo-system:number'),
         (NodeTypeRemoved, '/yolo-system:state/yolo-system:number'),
+        (SNodeAdded, '/yolo-system:conf/yolo-system:url/yolo-system:enabled'),
+        (SNodeAdded, '/yolo-system:state/yolo-system:url/yolo-system:enabled'),
         (StatusAdded, '/yolo-system:conf/yolo-system:deprecated-leaf'),
         (StatusAdded, '/yolo-system:conf/yolo-system:obsolete-leaf'),
         (StatusAdded, '/yolo-system:state/yolo-system:deprecated-leaf'),
@@ -47,6 +51,10 @@ class DiffTest(unittest.TestCase):
         ctx_new = Context(NEW_YANG_DIR)
         mod = ctx_new.load_module('yolo-system')
         mod.feature_enable_all()
-        diffs = frozenset((d.__class__, d.new.schema_path())
-                          for d in schema_diff(ctx_old, ctx_new))
-        self.assertEqual(diffs, self.expected_diffs)
+        diffs = []
+        for d in schema_diff(ctx_old, ctx_new):
+            if isinstance(d, (SNodeAdded, SNodeRemoved)):
+                diffs.append((d.__class__, d.node.schema_path()))
+            else:
+                diffs.append((d.__class__, d.new.schema_path()))
+        self.assertEqual(frozenset(diffs), self.expected_diffs)

tests/test_schema.py
@@ -229,11 +229,11 @@ class ListTest(unittest.TestCase):
 
     def test_list_iter(self):
         children = list(iter(self.list))
-        self.assertEqual(len(children), 4)
+        self.assertEqual(len(children), 5)
 
     def test_list_children_skip_keys(self):
         children = list(self.list.children(skip_keys=True))
-        self.assertEqual(len(children), 2)
+        self.assertEqual(len(children), 3)
 
     def test_list_parent(self):
         parent = self.list.parent()

tests/yang/yolo/yolo-system.yang
@@ -70,6 +70,9 @@ module yolo-system {
       leaf path {
         type string;
       }
+      leaf enabled {
+        type boolean;
+      }
     }
 
     leaf-list number {
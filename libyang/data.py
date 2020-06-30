CESNET/libyang-python

commit 49fd8ea4dc0d89659ef02c4964de19a7b1625ba1

Add data tree support
Add support for parsing various formats to DNode objects, constructing
DNode objects from scratch and serializing them back to various formats.

Add basic unit tests.

Signed-off-by: Robin Jarry <robin.jarry@6wind.com>

libyang/data.py
@@ -1,0 +1,360 @@
+# Copyright (c) 2020 6WIND S.A.
+# SPDX-License-Identifier: MIT
+
+from _libyang import ffi
+from _libyang import lib
+
+from .schema import Module
+from .schema import SContainer
+from .schema import SLeaf
+from .schema import SLeafList
+from .schema import SList
+from .schema import SNode
+from .schema import SRpc
+from .schema import Type
+from .util import c2str
+from .util import str2c
+
+
+#------------------------------------------------------------------------------
+class PrintFmt:  # TODO: use enum when dropping python2 support
+    XML = lib.LYD_XML
+    JSON = lib.LYD_JSON
+    LYB = lib.LYD_LYB
+
+
+#------------------------------------------------------------------------------
+class PrintOpt:  # TODO: use enum when dropping python2 support
+    PRETTY = lib.LYP_FORMAT
+
+
+#------------------------------------------------------------------------------
+class PathOpt:  # TODO: use enum when dropping python2 support
+    UPDATE = lib.LYD_PATH_OPT_UPDATE
+    OUTPUT = lib.LYD_PATH_OPT_OUTPUT
+    NOPARENTRET = lib.LYD_PATH_OPT_NOPARENTRET
+
+
+#------------------------------------------------------------------------------
+class ParserOpt:  # TODO: use enum when dropping python2 support
+    DATA = lib.LYD_OPT_DATA
+    CONFIG = lib.LYD_OPT_CONFIG
+    STRICT = lib.LYD_OPT_STRICT
+    TRUSTED = lib.LYD_OPT_TRUSTED
+    NO_YANGLIB = lib.LYD_OPT_DATA_NO_YANGLIB
+
+
+#------------------------------------------------------------------------------
+class DNode(object):
+    """
+    Data tree node.
+    """
+    def __init__(self, context, node_p, autofree=False):
+        """
+        :arg Context context:
+            The libyang.Context python object.
+        :arg struct lyd_node * node_p:
+            The pointer to the C structure allocated by libyang.so.
+        :arg bool autofree:
+            If True, automatically call lyd_free_withsiblings when the last
+            reference to this object is lost.
+        """
+        self.context = context
+        self._node = ffi.cast('struct lyd_node *', node_p)
+        if autofree:
+            self._node = ffi.gc(self._node, lib.lyd_free_withsiblings)
+
+    def name(self):
+        return c2str(self._node.schema.name)
+
+    def module(self):
+        mod = lib.lyd_node_module(self._node)
+        if not mod:
+            raise self.context.error('cannot get module')
+        return Module(self.context, mod)
+
+    def schema(self):
+        return SNode.new(self.context, self._node.schema)
+
+    def default(self):
+        return bool(self._node.dflt)
+
+    def parent(self):
+        if not self._node.parent:
+            return None
+        return self.new(self.context, self._node.parent)
+
+    def find_one(self, xpath):
+        try:
+            return next(self.find_all(xpath))
+        except StopIteration:
+            return None
+
+    def find_all(self, xpath):
+        node_set = lib.lyd_find_path(self._node, str2c(xpath))
+        if not node_set:
+            raise self.context.error('cannot find path: %s', xpath)
+        try:
+            for i in range(node_set.number):
+                yield DNode.new(self.context, node_set.d[i])
+        finally:
+            lib.ly_set_free(node_set)
+
+    def path(self):
+        path = lib.lyd_path(self._node)
+        try:
+            return c2str(path)
+        finally:
+            lib.free(path)
+
+    def validate(self, flags=0):
+        node_p = ffi.new('struct lyd_node **')
+        node_p[0] = self._node
+        ret = lib.lyd_validate(node_p, flags, ffi.NULL)
+        if ret != 0:
+            self.context.error('validation failed')
+
+    def dump_str(self, fmt=PrintFmt.JSON, flags=0):
+        buf = ffi.new('char **')
+        ret = lib.lyd_print_mem(buf, self._node, fmt, flags)
+        if ret != 0:
+            raise self.context.error('cannot print node')
+        try:
+            return c2str(buf[0])
+        finally:
+            lib.free(buf[0])
+
+    def dump_file(self, fileobj, fmt=PrintFmt.JSON, flags=0):
+        ret = lib.lyd_print_fd(fileobj.fileno(), self._node, fmt, flags)
+        if ret != 0:
+            raise self.context.error('cannot print node')
+
+    def free(self, with_siblings=True):
+        try:
+            ffi.gc(self._node, None)  # disable automatic gc
+        except TypeError:
+            pass
+        try:
+            if with_siblings:
+                lib.lyd_free_withsiblings(self._node)
+            else:
+                lib.lyd_free(self._node)
+        finally:
+            self._node = None
+
+    def __repr__(self):
+        cls = self.__class__
+        return '<%s.%s: %s>' % (cls.__module__, cls.__name__, str(self))
+
+    def __str__(self):
+        return self.name()
+
+    NODETYPE_CLASS = {}
+
+    @classmethod
+    def register(cls, *nodetypes):
+        def _decorator(nodeclass):
+            for t in nodetypes:
+                cls.NODETYPE_CLASS[t] = nodeclass
+            return nodeclass
+        return _decorator
+
+    @classmethod
+    def new(cls, context, node_p, autofree=False):
+        node_p = ffi.cast('struct lyd_node *', node_p)
+        nodecls = cls.NODETYPE_CLASS.get(node_p.schema.nodetype, DNode)
+        return nodecls(context, node_p, autofree=autofree)
+
+
+#------------------------------------------------------------------------------
+@DNode.register(SNode.CONTAINER)
+class DContainer(DNode):
+
+    def create_path(self, path, value=None, flags=0):
+        return self.context.create_data_path(
+            path, parent=self, value=value, flags=flags)
+
+    def children(self):
+        child = self._node.child
+        while child:
+            yield DNode.new(self.context, child)
+            child = child.next
+
+    def __iter__(self):
+        return self.children()
+
+
+#------------------------------------------------------------------------------
+@DNode.register(SNode.RPC)
+class DRpc(DContainer):
+    pass
+
+
+#------------------------------------------------------------------------------
+@DNode.register(SNode.LIST)
+class DList(DContainer):
+    pass
+
+
+#------------------------------------------------------------------------------
+@DNode.register(SNode.LEAF)
+class DLeaf(DNode):
+
+    def __init__(self, context, node_p, autofree=False):
+        DNode.__init__(self, context, node_p, autofree=autofree)
+        self._leaf = ffi.cast('struct lyd_node_leaf_list *', node_p)
+
+    def set_value(self, new_val):
+        if new_val is not None:
+            if isinstance(new_val, bool):
+                new_val = str(new_val).lower()
+            elif not isinstance(new_val, str):
+                new_val = str(new_val)
+        if lib.lyd_change_leaf(self._leaf, str2c(new_val)) < 0:
+            raise self.context.error('failed to change leaf value')
+
+    def value(self):
+        if self._leaf.value_type == Type.EMPTY:
+            return None
+        if self._leaf.value_type in Type.NUM_TYPES:
+            return int(c2str(self._leaf.value_str))
+        if self._leaf.value_type in (
+                Type.STRING, Type.BINARY, Type.ENUM, Type.IDENT, Type.BITS):
+            return c2str(self._leaf.value_str)
+        if self._leaf.value_type == Type.DEC64:
+            return lib.lyd_dec64_to_double(self._node)
+        if self._leaf.value_type == Type.LEAFREF:
+            referenced = DNode.new(self.context, self._leaf.value.leafref)
+            return referenced.value()
+        if self._leaf.value_type == Type.BOOL:
+            return bool(self._leaf.value.bln)
+
+
+#------------------------------------------------------------------------------
+@DNode.register(SNode.LEAFLIST)
+class DLeafList(DLeaf):
+    pass
+
+
+#------------------------------------------------------------------------------
+def dnode_to_dict(dnode, strip_prefixes=True):
+    """
+    Convert a DNode object to a python dictionary.
+
+    :arg DNode dnode:
+        The data node to convert.
+    :arg bool strip_prefixes:
+        If True (the default), module prefixes are stripped from dictionary
+        keys. If False, dictionary keys are in the form ``<module>:<name>``.
+    """
+    def _to_dict(node, parent_dic):
+        if strip_prefixes:
+            name = node.name()
+        else:
+            name = '%s:%s' % (node.module().name(), node.name())
+        if isinstance(node, DList):
+            list_element = {}
+            for child in node:
+                _to_dict(child, list_element)
+            parent_dic.setdefault(name, []).append(list_element)
+        elif isinstance(node, (DContainer, DRpc)):
+            container = {}
+            for child in node:
+                _to_dict(child, container)
+            parent_dic[name] = container
+        elif isinstance(node, DLeafList):
+            parent_dic.setdefault(name, []).append(node.value())
+        elif isinstance(node, DLeaf):
+            parent_dic[name] = node.value()
+
+    dic = {}
+    _to_dict(dnode, dic)
+    return dic
+
+
+#------------------------------------------------------------------------------
+def dict_to_dnode(dic, schema, parent=None, rpc_input=False, rpc_output=False):
+    """
+    Convert a python dictionary to a DNode object given a YANG schema object.
+    The returned value is always a top-level data node (i.e.: without parent).
+
+    :arg dict dic:
+        The python dictionary to convert.
+    :arg SNode or Module schema:
+        The libyang schema object associated with the dictionary. It must be
+        at the same "level" than the dictionary (i.e.: direct children names of
+        the schema object must be keys in the dictionary).
+    :arg DNode parent:
+        Optional parent to update. If not specified a new top-level DNode will
+        be created.
+    :arg bool rpc_input:
+        If True, expect schema to be a SRpc object and dic will be parsed
+        by looking in the rpc input nodes.
+    :arg bool rpc_output:
+        If True, expect schema to be a SRpc object and dic will be parsed
+        by looking in the rpc output nodes.
+    """
+    if not dic:
+        return parent
+
+    flags = 0
+    if rpc_output:
+        flags |= PathOpt.OUTPUT
+
+    # XXX: ugly, required for python2. Cannot use nonlocal keyword
+    _parent = [parent]
+
+    def _to_dnode(_dic, _schema, key=()):
+        if isinstance(_schema, SRpc):
+            if rpc_input:
+                _schema = _schema.input()
+            elif rpc_output:
+                _schema = _schema.output()
+            else:
+                raise ValueError('rpc_input or rpc_output must be specified')
+        if not _schema:
+            return
+        for s in _schema:
+            name = s.name()
+            if name not in _dic:
+                name = s.fullname()
+                if name not in _dic:
+                    continue
+            d = _dic[name]
+            if isinstance(s, SContainer):
+                if s.presence():
+                    dnode = s.context.create_data_path(
+                        s.data_path() % key, parent=_parent[0], flags=flags)
+                    if _parent[0] is None:
+                        _parent[0] = dnode
+                _to_dnode(d, s, key)
+            elif isinstance(s, SList):
+                for element in d:
+                    next_key = tuple(
+                        element.get(k.name(), element.get(k.fullname()))
+                        for k in s.keys())
+                    _to_dnode(element, s, key + next_key)
+            elif isinstance(s, SLeafList):
+                for element in d:
+                    dnode = s.context.create_data_path(
+                        s.data_path() % key, parent=_parent[0],
+                        value=element, flags=flags)
+                    if _parent[0] is None:
+                        _parent[0] = dnode
+            elif isinstance(s, SLeaf):
+                dnode = s.context.create_data_path(
+                    s.data_path() % key, parent=_parent[0], value=d, flags=flags)
+                if _parent[0] is None:
+                    _parent[0] = dnode
+
+    _to_dnode(dic, schema)
+
+    # XXX: ugly, required for python2 support
+    parent = _parent[0]
+
+    if parent is not None:
+        # go back to the root of the created tree
+        while parent.parent() is not None:
+            parent = parent.parent()
+
+    return parent

tests/test_data.py
@@ -1,0 +1,220 @@
+# Copyright (c) 2020 6WIND S.A.
+# SPDX-License-Identifier: MIT
+
+import json
+import os
+import unittest
+
+from libyang import Context
+from libyang import LibyangError
+from libyang.data import DContainer
+from libyang.data import ParserOpt
+from libyang.data import PrintFmt
+from libyang.data import PrintOpt
+from libyang.data import dict_to_dnode
+from libyang.data import dnode_to_dict
+
+
+YANG_DIR = os.path.join(os.path.dirname(__file__), 'yang')
+
+
+#------------------------------------------------------------------------------
+class DataTest(unittest.TestCase):
+
+    def setUp(self):
+        self.ctx = Context(YANG_DIR)
+        mod = self.ctx.load_module('yolo-system')
+        mod.feature_enable_all()
+
+    def tearDown(self):
+        self.ctx = None
+
+    JSON_CONFIG = '''{
+  "yolo-system:conf": {
+    "hostname": "foo",
+    "speed": 1234,
+    "number": [
+      1000,
+      2000,
+      3000
+    ],
+    "url": [
+      {
+        "proto": "https",
+        "host": "github.com",
+        "path": "/rjarry/libyang-cffi"
+      },
+      {
+        "proto": "http",
+        "host": "foobar.com",
+        "port": 8080,
+        "path": "/index.html"
+      }
+    ]
+  }
+}
+'''
+
+    def test_data_parse_config_json(self):
+        dnode = self.ctx.parse_data_str(
+            self.JSON_CONFIG, PrintFmt.JSON, ParserOpt.CONFIG)
+        self.assertIsInstance(dnode, DContainer)
+        j = dnode.dump_str(PrintFmt.JSON, PrintOpt.PRETTY)
+        self.assertEqual(j, self.JSON_CONFIG)
+        dnode.free()
+
+    JSON_STATE = '''{
+  "yolo-system:state": {
+    "hostname": "foo",
+    "speed": 1234,
+    "number": [
+      1000,
+      2000,
+      3000
+    ],
+    "url": [
+      {
+        "proto": "https",
+        "host": "github.com",
+        "path": "/rjarry/libyang-cffi"
+      },
+      {
+        "proto": "http",
+        "host": "foobar.com",
+        "port": 8080,
+        "path": "/index.html"
+      }
+    ]
+  }
+}
+'''
+
+    def test_data_parse_state_json(self):
+        dnode = self.ctx.parse_data_str(
+            self.JSON_STATE, PrintFmt.JSON,
+            ParserOpt.DATA | ParserOpt.NO_YANGLIB)
+        self.assertIsInstance(dnode, DContainer)
+        j = dnode.dump_str(PrintFmt.JSON, PrintOpt.PRETTY)
+        self.assertEqual(j, self.JSON_STATE)
+        dnode.free()
+
+    XML_CONFIG = '''<conf xmlns="urn:yang:yolo:system">
+  <hostname>foo</hostname>
+  <speed>1234</speed>
+  <number>1000</number>
+  <number>2000</number>
+  <number>3000</number>
+  <url>
+    <proto>https</proto>
+    <host>github.com</host>
+    <path>/rjarry/libyang-cffi</path>
+  </url>
+  <url>
+    <proto>http</proto>
+    <host>foobar.com</host>
+    <port>8080</port>
+    <path>/index.html</path>
+  </url>
+</conf>
+'''
+
+    def test_data_parse_config_xml(self):
+        dnode = self.ctx.parse_data_str(
+            self.XML_CONFIG, PrintFmt.XML, ParserOpt.CONFIG)
+        self.assertIsInstance(dnode, DContainer)
+        xml = dnode.dump_str(PrintFmt.XML, PrintOpt.PRETTY)
+        self.assertEqual(xml, self.XML_CONFIG)
+        dnode.free()
+
+    XML_STATE = '''<state xmlns="urn:yang:yolo:system">
+  <hostname>foo</hostname>
+  <speed>1234</speed>
+  <number>1000</number>
+  <number>2000</number>
+  <number>3000</number>
+  <url>
+    <proto>https</proto>
+    <host>github.com</host>
+    <path>/rjarry/libyang-cffi</path>
+  </url>
+  <url>
+    <proto>http</proto>
+    <host>foobar.com</host>
+    <port>8080</port>
+    <path>/index.html</path>
+  </url>
+</state>
+'''
+
+    def test_data_parse_data_xml(self):
+        dnode = self.ctx.parse_data_str(
+            self.XML_STATE, PrintFmt.XML,
+            ParserOpt.DATA | ParserOpt.NO_YANGLIB)
+        self.assertIsInstance(dnode, DContainer)
+        xml = dnode.dump_str(PrintFmt.XML, PrintOpt.PRETTY)
+        self.assertEqual(xml, self.XML_STATE)
+        dnode.free()
+
+    def test_data_create_paths(self):
+        state = self.ctx.create_data_path('/yolo-system:state')
+        state.create_path('hostname', 'foo')
+        state.create_path('speed', 1234)
+        state.create_path('number', 1000)
+        state.create_path('number', 2000)
+        state.create_path('number', 3000)
+        u = state.create_path('url[proto="https"][host="github.com"]')
+        u.create_path('path', '/rjarry/libyang-cffi')
+        u = state.create_path('url[proto="http"][host="foobar.com"]')
+        u.create_path('port', 8080)
+        u.create_path('path', '/index.html')
+        state.validate(ParserOpt.STRICT)
+        self.assertEqual(state.dump_str(PrintFmt.JSON, PrintOpt.PRETTY), self.JSON_STATE)
+        state.free()
+
+    def test_data_create_invalid_type(self):
+        s = self.ctx.create_data_path('/yolo-system:state')
+        with self.assertRaises(LibyangError):
+            s.create_path('speed', 1234000000000000000000000000)
+        s.free()
+
+    def test_data_create_invalid_regexp(self):
+        s = self.ctx.create_data_path('/yolo-system:state')
+        with self.assertRaises(LibyangError):
+            s.create_path('hostname', 'INVALID.HOST')
+        s.free()
+
+    DICT_CONFIG = {
+        'conf': {
+            'hostname': 'foo',
+            'speed': 1234,
+            'number': [1000, 2000, 3000],
+            'url': [
+                {
+                    'proto': 'https',
+                    'host': 'github.com',
+                    'path': '/rjarry/libyang-cffi',
+                },
+                {
+                    'proto': 'http',
+                    'host': 'foobar.com',
+                    'port': 8080,
+                    'path': '/index.html',
+                },
+            ],
+        },
+    }
+
+    def test_data_to_dict(self):
+        dnode = self.ctx.parse_data_str(
+            self.JSON_CONFIG, PrintFmt.JSON, ParserOpt.CONFIG)
+        self.assertIsInstance(dnode, DContainer)
+        dic = dnode_to_dict(dnode)
+        dnode.free()
+        self.assertEqual(dic, self.DICT_CONFIG)
+
+    def test_data_from_dict(self):
+        schema = self.ctx.get_module('yolo-system')
+        dnode = dict_to_dnode(self.DICT_CONFIG, schema)
+        j = dnode.dump_str(PrintFmt.JSON, PrintOpt.PRETTY)
+        dnode.free()
+        self.assertEqual(json.loads(j), json.loads(self.JSON_CONFIG))

cffi/cdefs.h
@@ -17,6 +17,11 @@ int ly_ctx_set_searchdir(struct ly_ctx *, const char *);
 void ly_ctx_destroy(struct ly_ctx *, void *);
 
 typedef enum {
+	LY_SUCCESS,
+	...
+} LY_ERR;
+
+typedef enum {
 	LY_LLERR,
 	LY_LLWRN,
 	LY_LLVRB,
@@ -349,6 +354,69 @@ typedef enum {
 int lys_print_mem(char **, const struct lys_module *, LYS_OUTFORMAT, const char *, int, int);
 int lys_print_fd(int, const struct lys_module *, LYS_OUTFORMAT, const char *, int, int);
 
+typedef enum {
+	LYD_XML,
+	LYD_JSON,
+	LYD_LYB,
+	...
+} LYD_FORMAT;
+
+typedef enum {
+	...
+} LYD_ANYDATA_VALUETYPE;
+
+#define LYP_FORMAT ...
+
+#define LYD_ANYDATA_CONSTSTRING ...
+
+#define LYD_PATH_OPT_UPDATE ...
+#define LYD_PATH_OPT_NOPARENTRET ...
+#define LYD_PATH_OPT_OUTPUT ...
+
+#define LYD_OPT_DATA ...
+#define LYD_OPT_CONFIG ...
+#define LYD_OPT_STRICT ...
+#define LYD_OPT_TRUSTED ...
+#define LYD_OPT_DATA_NO_YANGLIB ...
+
+typedef union lyd_value_u {
+	int8_t bln;
+	struct lyd_node *leafref;
+	...;
+} lyd_val;
+
+struct lyd_node {
+	struct lys_node *schema;
+	struct lyd_node *next;
+	struct lyd_node *child;
+	struct lyd_node *parent;
+	...;
+};
+
+struct lyd_node_leaf_list {
+	struct lys_node *schema;
+	struct lyd_node *next;
+	struct lyd_node *parent;
+	const char *value_str;
+	lyd_val value;
+	LY_DATA_TYPE value_type;
+	...;
+};
+
+struct ly_set *lyd_find_instance(const struct lyd_node *, const struct lys_node *);
+struct ly_set *lyd_find_path(const struct lyd_node *, const char *);
+struct lyd_node *lyd_new_path(struct lyd_node *, const struct ly_ctx *, const char *, void *, LYD_ANYDATA_VALUETYPE, int);
+char *lyd_path(const struct lyd_node *);
+struct lys_module *lyd_node_module(struct lyd_node *);
+struct lyd_node *lyd_parse_mem(struct ly_ctx *, const char *, LYD_FORMAT, int);
+struct lyd_node *lyd_parse_fd(struct ly_ctx *, int, LYD_FORMAT, int);
+int lyd_print_fd(int, const struct lyd_node *, LYD_FORMAT, int);
+int lyd_print_mem(char **, const struct lyd_node *, LYD_FORMAT, int);
+double lyd_dec64_to_double(const struct lyd_node *);
+void lyd_free(struct lyd_node *);
+void lyd_free_withsiblings(struct lyd_node *);
+int lyd_validate(struct lyd_node **, int, void *);
+
 /* from libc, needed to free allocated strings */
 void free(void *);
 
@@ -358,3 +426,5 @@ const struct lys_ext_instance *lypy_find_ext(
 	const char *, const char *, const char *);
 char *lypy_data_path_pattern(const struct lys_node *);
 char *lypy_node_fullname(const struct lys_node *);
+LY_ERR lypy_get_errno(void);
+void lypy_set_errno(LY_ERR);

cffi/source.c
@@ -116,3 +116,13 @@ static char *lypy_node_fullname(const struct lys_node *node)
 
 	return fullname;
 }
+
+static LY_ERR lypy_get_errno(void)
+{
+	return ly_errno;
+}
+
+static void lypy_set_errno(LY_ERR err)
+{
+	ly_errno = err;
+}

libyang/__init__.py
@@ -1,4 +1,5 @@
 # Copyright (c) 2018-2019 Robin Jarry
+# Copyright (c) 2020 6WIND S.A.
 # SPDX-License-Identifier: MIT
 
 import os  # isort:skip
@@ -18,6 +19,8 @@ import logging
 from _libyang import ffi
 from _libyang import lib
 
+from .data import DNode
+from .data import PathOpt
 from .schema import Module
 from .schema import SNode
 from .util import LibyangError
@@ -103,6 +106,46 @@ class Context(object):
                 yield SNode.new(self, node_set.set.s[i])
         finally:
             lib.ly_set_free(node_set)
+
+    def create_data_path(self, path, parent=None, value=None, flags=0):
+        lib.lypy_set_errno(0)
+        if value is not None and not isinstance(value, str):
+            value = str(value)
+        dnode = lib.lyd_new_path(
+            parent._node if parent else ffi.NULL,
+            self._ctx, str2c(path), str2c(value), 0,
+            PathOpt.UPDATE | PathOpt.NOPARENTRET | flags)
+        if lib.lypy_get_errno() != 0:
+            raise self.error('cannot create data path: %s', path)
+
+        if not dnode and parent:
+            # This can happen when path points to an already created leaf and
+            # its value does not change.
+            # In that case, lookup the existing leaf and return it.
+            node_set = lib.lyd_find_path(parent._node, str2c(path))
+            try:
+                if not node_set or not node_set.number:
+                    raise self.error('cannot find path')
+                dnode = node_set.set.s[0]
+            finally:
+                lib.ly_set_free(node_set)
+
+        if not dnode:
+            raise self.error('cannot find created path')
+
+        return DNode.new(self, dnode)
+
+    def parse_data_str(self, s, fmt, flags=0):
+        dnode = lib.lyd_parse_mem(self._ctx, str2c(s), fmt, flags)
+        if not dnode:
+            raise self.error('failed to parse data tree')
+        return DNode.new(self, dnode)
+
+    def parse_data_file(self, fileobj, fmt, flags=0):
+        dnode = lib.lyd_parse_fd(self._ctx, fileobj.fileno(), fmt, flags)
+        if not dnode:
+            raise self.error('failed to parse data tree')
+        return DNode.new(self, dnode)
 
     def __iter__(self):
         """

tests/yang-old/wtf/wtf-types.yang
@@ -7,7 +7,9 @@ module wtf-types {
   }
 
   typedef host {
-    type str;
+    type str {
+      pattern "[a-z]+";
+    }
   }
 
   typedef unsigned {

tests/yang/wtf/wtf-types.yang
@@ -7,7 +7,9 @@ module wtf-types {
   }
 
   typedef host {
-    type str;
+    type str {
+      pattern "[a-z]+";
+    }
   }
 
   typedef unsigned {

commit f9f71bbdb14d0a8a42a17f3447e1724be029bacb

data: add missing raise statement
Without this, no error is risen to the caller.

Fixes: 49fd8ea4dc0d ("Add data tree support")
Signed-off-by: Robin Jarry <robin@jarry.cc>

libyang/data.py
@@ -178,7 +178,7 @@ class DNode:
         node_p[0] = self._node
         ret = lib.lyd_validate(node_p, flags, ffi.NULL)
         if ret != 0:
-            self.context.error('validation failed')
+            raise self.context.error('validation failed')
 
     def merge(self, source, destruct=False, no_siblings=False, explicit=False):
         flags = parser_flags(destruct=destruct, no_siblings=no_siblings,

tests/test_data.py
@@ -348,7 +348,8 @@ class DataTest(unittest.TestCase):
     def test_data_from_dict_rpc(self):
         dnode = self.ctx.create_data_path('/yolo-system:format-disk')
         self.assertIsInstance(dnode, DRpc)
-        dnode.merge_data_dict({'duration': 42}, rpcreply=True, strict=True)
+        dnode.merge_data_dict(
+            {'duration': 42}, rpcreply=True, strict=True, no_yanglib=True)
         try:
             j = dnode.print_mem('json')
         finally:
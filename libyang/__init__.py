CESNET/libyang-python

commit 05ee0d8b09e1e13ba3c77257111ad6ee27a4fece

data: allow parsing rpc input
Signed-off-by: Robin Jarry <robin@jarry.cc>

libyang/__init__.py
@@ -177,18 +177,21 @@ class Context(object):
         return DNode.new(self, dnode)
 
     def parse_data_mem(self, d, fmt, data=False, config=False, strict=False,
-                       trusted=False, no_yanglib=False):
+                       trusted=False, no_yanglib=False, rpc=False):
         if self._ctx is None:
             raise RuntimeError('context already destroyed')
         flags = parser_flags(
             data=data, config=config, strict=strict, trusted=trusted,
-            no_yanglib=no_yanglib)
+            no_yanglib=no_yanglib, rpc=rpc)
         fmt = data_format(fmt)
         if fmt == lib.LYD_LYB:
             d = str2c(d, encode=False)
         else:
             d = str2c(d, encode=True)
-        dnode = lib.lyd_parse_mem(self._ctx, d, fmt, flags)
+        args = []
+        if rpc:
+            args.append(ffi.cast('struct lyd_node *', ffi.NULL))
+        dnode = lib.lyd_parse_mem(self._ctx, d, fmt, flags, *args)
         if not dnode:
             raise self.error('failed to parse data tree')
         return DNode.new(self, dnode)
@@ -201,7 +204,10 @@ class Context(object):
             data=data, config=config, strict=strict, trusted=trusted,
             no_yanglib=no_yanglib)
         fmt = data_format(fmt)
-        dnode = lib.lyd_parse_fd(self._ctx, fileobj.fileno(), fmt, flags)
+        args = []
+        if rpc:
+            args.append(ffi.cast('struct lyd_node *', ffi.NULL))
+        dnode = lib.lyd_parse_fd(self._ctx, fileobj.fileno(), fmt, flags, *args)
         if not dnode:
             raise self.error('failed to parse data tree')
         return DNode.new(self, dnode)

cffi/cdefs.h
@@ -392,6 +392,7 @@ typedef enum {
 #define LYD_OPT_STRICT ...
 #define LYD_OPT_TRUSTED ...
 #define LYD_OPT_DATA_NO_YANGLIB ...
+#define LYD_OPT_RPC ...
 
 typedef union lyd_value_u {
 	int8_t bln;
@@ -423,8 +424,8 @@ struct lyd_node *lyd_new_path(struct lyd_node *, const struct ly_ctx *, const ch
 struct lyd_node *lyd_first_sibling(struct lyd_node *);
 char *lyd_path(const struct lyd_node *);
 struct lys_module *lyd_node_module(struct lyd_node *);
-struct lyd_node *lyd_parse_mem(struct ly_ctx *, const char *, LYD_FORMAT, int);
-struct lyd_node *lyd_parse_fd(struct ly_ctx *, int, LYD_FORMAT, int);
+struct lyd_node *lyd_parse_mem(struct ly_ctx *, const char *, LYD_FORMAT, int, ...);
+struct lyd_node *lyd_parse_fd(struct ly_ctx *, int, LYD_FORMAT, int, ...);
 int lyd_print_fd(int, const struct lyd_node *, LYD_FORMAT, int);
 int lyd_print_mem(char **, const struct lyd_node *, LYD_FORMAT, int);
 double lyd_dec64_to_double(const struct lyd_node *);

libyang/data.py
@@ -58,7 +58,7 @@ def path_flags(update=False, rpc_output=False, no_parent_ret=False):
 
 #------------------------------------------------------------------------------
 def parser_flags(data=False, config=False, strict=False, trusted=False,
-                 no_yanglib=False):
+                 no_yanglib=False, rpc=False):
     flags = 0
     if data:
         flags |= lib.LYD_OPT_DATA
@@ -70,6 +70,8 @@ def parser_flags(data=False, config=False, strict=False, trusted=False,
         flags |= lib.LYD_OPT_TRUSTED
     if no_yanglib:
         flags |= lib.LYD_OPT_DATA_NO_YANGLIB
+    if rpc:
+        flags |= lib.LYD_OPT_RPC
     return flags
 
 

commit 9b989111d24db7a4c863993b55bdc254311873cc

parse_data_file: add missing rpc flag
This flag is available for parse_data_mem, add it for parse_data_file as
well.

Fixes: 05ee0d8b09e1 ("data: allow parsing rpc input")
Signed-off-by: Robin Jarry <robin@jarry.cc>

libyang/__init__.py
@@ -205,12 +205,12 @@ class Context:
         return DNode.new(self, dnode)
 
     def parse_data_file(self, fileobj, fmt, data=False, config=False,
-                        strict=False, trusted=False, no_yanglib=False):
+                        strict=False, trusted=False, no_yanglib=False, rpc=False):
         if self._ctx is None:
             raise RuntimeError('context already destroyed')
         flags = parser_flags(
             data=data, config=config, strict=strict, trusted=trusted,
-            no_yanglib=no_yanglib)
+            no_yanglib=no_yanglib, rpc=rpc)
         fmt = data_format(fmt)
         args = []
         if rpc:
9b989111d24db7a4c863993b55bdc254311873cc
Robin Jarry <robin@jarry.cc>
2020-06-30 12:27:06 +0200

    parse_data_file: add missing rpc flag
    This flag is available for parse_data_mem, add it for parse_data_file as
    well.
    
    Fixes: 05ee0d8b09e1 ("data: allow parsing rpc input")
    Signed-off-by: Robin Jarry <robin@jarry.cc>

--- libyang/__init__.py
+++ libyang/__init__.py
@@ -205,12 +205,12 @@ class Context:
         return DNode.new(self, dnode)
 
     def parse_data_file(self, fileobj, fmt, data=False, config=False,
-                        strict=False, trusted=False, no_yanglib=False):
+                        strict=False, trusted=False, no_yanglib=False, rpc=False):
         if self._ctx is None:
             raise RuntimeError('context already destroyed')
         flags = parser_flags(
             data=data, config=config, strict=strict, trusted=trusted,
-            no_yanglib=no_yanglib)
+            no_yanglib=no_yanglib, rpc=rpc)
         fmt = data_format(fmt)
         args = []
         if rpc:

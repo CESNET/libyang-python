CESNET/libyang-python

commit 6dae8ee38fb3c80149a2a9a1ba07c50494dcd43c

context: allow explicitly destroying
Instead of automatically calling ly_ctx_destroy when the last reference
to self._ctx is lost, add a destroy() method. This method can be called
multiple times without risking a "double free".

Add context manager methods so that Context() can be used in with
constructs:

  with Context() as ctx:
      # do stuff with ctx
  # ctx has been destroyed

After destroy() has been called the context may not be used anymore.

Signed-off-by: Robin Jarry <robin@jarry.cc>

libyang/__init__.py
@@ -47,8 +47,7 @@ class Context(object):
         if disable_searchdir_cwd:
             options |= lib.LY_CTX_DISABLE_SEARCHDIR_CWD
 
-        self._ctx = ffi.gc(lib.ly_ctx_new(ffi.NULL, options),
-                           lambda c: lib.ly_ctx_destroy(c, ffi.NULL))
+        self._ctx = lib.ly_ctx_new(ffi.NULL, options)
         if not self._ctx:
             raise self.error('cannot create context')
 
@@ -68,20 +67,34 @@ class Context(object):
             if lib.ly_ctx_set_searchdir(self._ctx, str2c(path)) != 0:
                 raise self.error('cannot set search dir')
 
+    def destroy(self):
+        if self._ctx is not None:
+            lib.ly_ctx_destroy(self._ctx, ffi.NULL)
+            self._ctx = None
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, *args, **kwargs):
+        self.destroy()
+
     def error(self, msg, *args):
         msg %= args
 
-        err = lib.ly_err_first(self._ctx)
-        if err:
-            if err.msg:
-                msg += ': %s' % c2str(err.msg)
-            if err.path:
-                msg += ': %s' % c2str(err.path)
-        lib.ly_err_clean(self._ctx, ffi.NULL)
+        if self._ctx:
+            err = lib.ly_err_first(self._ctx)
+            if err:
+                if err.msg:
+                    msg += ': %s' % c2str(err.msg)
+                if err.path:
+                    msg += ': %s' % c2str(err.path)
+            lib.ly_err_clean(self._ctx, ffi.NULL)
 
         return LibyangError(msg)
 
     def parse_module_file(self, fileobj, fmt='yang'):
+        if self._ctx is None:
+            raise RuntimeError('context already destroyed')
         fmt = schema_in_format(fmt)
         mod = lib.lys_parse_fd(self._ctx, fileobj.fileno(), fmt)
         if not mod:
@@ -90,6 +103,8 @@ class Context(object):
         return Module(self, mod)
 
     def parse_module_str(self, s, fmt='yang'):
+        if self._ctx is None:
+            raise RuntimeError('context already destroyed')
         fmt = schema_in_format(fmt)
         mod = lib.lys_parse_mem(self._ctx, str2c(s), fmt)
         if not mod:
@@ -98,6 +113,8 @@ class Context(object):
         return Module(self, mod)
 
     def load_module(self, name):
+        if self._ctx is None:
+            raise RuntimeError('context already destroyed')
         mod = lib.ly_ctx_load_module(self._ctx, str2c(name), ffi.NULL)
         if not mod:
             raise self.error('cannot load module')
@@ -105,6 +122,8 @@ class Context(object):
         return Module(self, mod)
 
     def get_module(self, name):
+        if self._ctx is None:
+            raise RuntimeError('context already destroyed')
         mod = lib.ly_ctx_get_module(self._ctx, str2c(name), ffi.NULL, False)
         if not mod:
             raise self.error('cannot get module')
@@ -112,6 +131,8 @@ class Context(object):
         return Module(self, mod)
 
     def find_path(self, path):
+        if self._ctx is None:
+            raise RuntimeError('context already destroyed')
         node_set = lib.ly_ctx_find_path(self._ctx, str2c(path))
         if not node_set:
             raise self.error('cannot find path')
@@ -122,6 +143,8 @@ class Context(object):
             lib.ly_set_free(node_set)
 
     def create_data_path(self, path, parent=None, value=None, rpc_output=False):
+        if self._ctx is None:
+            raise RuntimeError('context already destroyed')
         lib.lypy_set_errno(0)
         if value is not None:
             if isinstance(value, bool):
@@ -155,6 +178,8 @@ class Context(object):
 
     def parse_data_str(self, s, fmt, data=False, config=False, strict=False,
                        trusted=False, no_yanglib=False):
+        if self._ctx is None:
+            raise RuntimeError('context already destroyed')
         flags = parser_flags(
             data=data, config=config, strict=strict, trusted=trusted,
             no_yanglib=no_yanglib)
@@ -166,6 +191,8 @@ class Context(object):
 
     def parse_data_file(self, fileobj, fmt, data=False, config=False,
                         strict=False, trusted=False, no_yanglib=False):
+        if self._ctx is None:
+            raise RuntimeError('context already destroyed')
         flags = parser_flags(
             data=data, config=config, strict=strict, trusted=trusted,
             no_yanglib=no_yanglib)
@@ -179,6 +206,8 @@ class Context(object):
         """
         Return an iterator that yields all implemented modules from the context
         """
+        if self._ctx is None:
+            raise RuntimeError('context already destroyed')
         idx = ffi.new('uint32_t *')
         mod = lib.ly_ctx_get_module_iter(self._ctx, idx)
         while mod:

tests/test_context.py
@@ -17,22 +17,22 @@ YANG_DIR = os.path.join(os.path.dirname(__file__), 'yang')
 class ContextTest(unittest.TestCase):
 
     def test_ctx_no_dir(self):
-        ctx = Context()
-        self.assertIsNot(ctx, None)
+        with Context() as ctx:
+            self.assertIsNot(ctx, None)
 
     def test_ctx_dir(self):
-        ctx = Context(YANG_DIR)
-        self.assertIsNot(ctx, None)
+        with Context(YANG_DIR) as ctx:
+            self.assertIsNot(ctx, None)
 
     def test_ctx_invalid_dir(self):
-        ctx = Context('/does/not/exist')
-        self.assertIsNot(ctx, None)
+        with Context('/does/not/exist') as ctx:
+            self.assertIsNot(ctx, None)
 
     def test_ctx_missing_dir(self):
-        ctx = Context(os.path.join(YANG_DIR, 'yolo'))
-        self.assertIsNot(ctx, None)
-        with self.assertRaises(LibyangError):
-            ctx.load_module('yolo-system')
+        with Context(os.path.join(YANG_DIR, 'yolo')) as ctx:
+            self.assertIsNot(ctx, None)
+            with self.assertRaises(LibyangError):
+                ctx.load_module('yolo-system')
 
     def test_ctx_env_search_dir(self):
         try:
@@ -40,42 +40,42 @@ class ContextTest(unittest.TestCase):
                 os.path.join(YANG_DIR, 'omg'),
                 os.path.join(YANG_DIR, 'wtf'),
                 ])
-            ctx = Context(os.path.join(YANG_DIR, 'yolo'))
-            mod = ctx.load_module('yolo-system')
-            self.assertIsInstance(mod, Module)
+            with Context(os.path.join(YANG_DIR, 'yolo')) as ctx:
+                mod = ctx.load_module('yolo-system')
+                self.assertIsInstance(mod, Module)
         finally:
             del os.environ['YANGPATH']
 
     def test_ctx_load_module(self):
-        ctx = Context(YANG_DIR)
-        mod = ctx.load_module('yolo-system')
-        self.assertIsInstance(mod, Module)
+        with Context(YANG_DIR) as ctx:
+            mod = ctx.load_module('yolo-system')
+            self.assertIsInstance(mod, Module)
 
     def test_ctx_get_module(self):
-        ctx = Context(YANG_DIR)
-        ctx.load_module('yolo-system')
-        mod = ctx.get_module('wtf-types')
-        self.assertIsInstance(mod, Module)
+        with Context(YANG_DIR) as ctx:
+            ctx.load_module('yolo-system')
+            mod = ctx.get_module('wtf-types')
+            self.assertIsInstance(mod, Module)
 
     def test_ctx_get_invalid_module(self):
-        ctx = Context(YANG_DIR)
-        ctx.load_module('wtf-types')
-        with self.assertRaises(LibyangError):
-            ctx.get_module('yolo-system')
+        with Context(YANG_DIR) as ctx:
+            ctx.load_module('wtf-types')
+            with self.assertRaises(LibyangError):
+                ctx.get_module('yolo-system')
 
     def test_ctx_load_invalid_module(self):
-        ctx = Context(YANG_DIR)
-        with self.assertRaises(LibyangError):
-            ctx.load_module('invalid-module')
+        with Context(YANG_DIR) as ctx:
+            with self.assertRaises(LibyangError):
+                ctx.load_module('invalid-module')
 
     def test_ctx_find_path(self):
-        ctx = Context(YANG_DIR)
-        ctx.load_module('yolo-system')
-        node = next(ctx.find_path('/yolo-system:format-disk'))
-        self.assertIsInstance(node, SRpc)
+        with Context(YANG_DIR) as ctx:
+            ctx.load_module('yolo-system')
+            node = next(ctx.find_path('/yolo-system:format-disk'))
+            self.assertIsInstance(node, SRpc)
 
     def test_ctx_iter_modules(self):
-        ctx = Context(YANG_DIR)
-        ctx.load_module('yolo-system')
-        modules = list(iter(ctx))
-        self.assertGreater(len(modules), 0)
+        with Context(YANG_DIR) as ctx:
+            ctx.load_module('yolo-system')
+            modules = list(iter(ctx))
+            self.assertGreater(len(modules), 0)

tests/test_data.py
@@ -24,6 +24,7 @@ class DataTest(unittest.TestCase):
         mod.feature_enable_all()
 
     def tearDown(self):
+        self.ctx.destroy()
         self.ctx = None
 
     JSON_CONFIG = '''{

tests/test_diff.py
@@ -45,16 +45,15 @@ class DiffTest(unittest.TestCase):
     ))
 
     def test_diff(self):
-        ctx_old = Context(OLD_YANG_DIR)
-        mod = ctx_old.load_module('yolo-system')
-        mod.feature_enable_all()
-        ctx_new = Context(NEW_YANG_DIR)
-        mod = ctx_new.load_module('yolo-system')
-        mod.feature_enable_all()
-        diffs = []
-        for d in schema_diff(ctx_old, ctx_new):
-            if isinstance(d, (SNodeAdded, SNodeRemoved)):
-                diffs.append((d.__class__, d.node.schema_path()))
-            else:
-                diffs.append((d.__class__, d.new.schema_path()))
-        self.assertEqual(frozenset(diffs), self.expected_diffs)
+        with Context(OLD_YANG_DIR) as ctx_old, Context(NEW_YANG_DIR) as ctx_new:
+            mod = ctx_old.load_module('yolo-system')
+            mod.feature_enable_all()
+            mod = ctx_new.load_module('yolo-system')
+            mod.feature_enable_all()
+            diffs = []
+            for d in schema_diff(ctx_old, ctx_new):
+                if isinstance(d, (SNodeAdded, SNodeRemoved)):
+                    diffs.append((d.__class__, d.node.schema_path()))
+                else:
+                    diffs.append((d.__class__, d.new.schema_path()))
+            self.assertEqual(frozenset(diffs), self.expected_diffs)

tests/test_schema.py
@@ -32,6 +32,7 @@ class ModuleTest(unittest.TestCase):
 
     def tearDown(self):
         self.module = None
+        self.ctx.destroy()
         self.ctx = None
 
     def test_mod_dump_str(self):
@@ -102,6 +103,7 @@ class RevisionTest(unittest.TestCase):
 
     def tearDown(self):
         self.revision = None
+        self.ctx.destroy()
         self.ctx = None
 
     def test_rev_date(self):
@@ -133,6 +135,7 @@ class IfFeatureTest(unittest.TestCase):
 
     def tearDown(self):
         self.container = None
+        self.ctx.destroy()
         self.ctx = None
 
     def test_iffeatures(self):
@@ -171,6 +174,7 @@ class ContainerTest(unittest.TestCase):
 
     def tearDown(self):
         self.container = None
+        self.ctx.destroy()
         self.ctx = None
 
     def test_cont_attrs(self):
@@ -213,6 +217,7 @@ class ListTest(unittest.TestCase):
 
     def tearDown(self):
         self.list = None
+        self.ctx.destroy()
         self.ctx = None
 
     def test_list_attrs(self):
@@ -252,6 +257,7 @@ class RpcTest(unittest.TestCase):
 
     def tearDown(self):
         self.rpc = None
+        self.ctx.destroy()
         self.ctx = None
 
     def test_rpc_attrs(self):
@@ -285,6 +291,7 @@ class LeafTypeTest(unittest.TestCase):
         self.ctx.load_module('yolo-system')
 
     def tearDown(self):
+        self.ctx.destroy()
         self.ctx = None
 
     def test_leaf_type_derived(self):

commit 7824eca4545f9a3729bc62fdaaa6be53e8bb9765

context: report all stored errors
Do not report only the first one as we might miss some.

Fixes: 6dae8ee38fb3 ("context: allow explicitly destroying")
Signed-off-by: Robin Jarry <robin@jarry.cc>

libyang/__init__.py
@@ -83,11 +83,12 @@ class Context:
 
         if self._ctx:
             err = lib.ly_err_first(self._ctx)
-            if err:
+            while err:
                 if err.msg:
                     msg += ': %s' % c2str(err.msg)
                 if err.path:
                     msg += ': %s' % c2str(err.path)
+                err = err.next
             lib.ly_err_clean(self._ctx, ffi.NULL)
 
         return LibyangError(msg)
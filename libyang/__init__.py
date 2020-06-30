CESNET/libyang-python

commit 1b4f76b121094fa04d0c39f5cc705e4a02a47fdf

data: add options to create_data_path
Signed-off-by: Robin Jarry <robin@jarry.cc>

libyang/__init__.py
@@ -142,7 +142,9 @@ class Context(object):
         finally:
             lib.ly_set_free(node_set)
 
-    def create_data_path(self, path, parent=None, value=None, rpc_output=False):
+    def create_data_path(self, path, parent=None, value=None,
+                         update=True, no_parent_ret=True, rpc_output=False,
+                         force_return_value=True):
         if self._ctx is None:
             raise RuntimeError('context already destroyed')
         lib.lypy_set_errno(0)
@@ -152,12 +154,15 @@ class Context(object):
             elif not isinstance(value, str):
                 value = str(value)
         flags = path_flags(
-            update=True, no_parent_ret=True, rpc_output=rpc_output)
+            update=update, no_parent_ret=no_parent_ret, rpc_output=rpc_output)
         dnode = lib.lyd_new_path(
             parent._node if parent else ffi.NULL,
             self._ctx, str2c(path), str2c(value), 0, flags)
-        if lib.lypy_get_errno() != 0:
-            raise self.error('cannot create data path')
+        if lib.lypy_get_errno() != lib.LY_SUCCESS:
+            if lib.ly_vecode(self._ctx) != lib.LYVE_PATH_EXISTS:
+                raise self.error('cannot create data path')
+        if not dnode and not force_return_value:
+            return None
 
         if not dnode and parent:
             # This can happen when path points to an already created leaf and

cffi/cdefs.h
@@ -22,6 +22,12 @@ typedef enum {
 } LY_ERR;
 
 typedef enum {
+    LYVE_SUCCESS,
+    LYVE_PATH_EXISTS,
+    ...
+} LY_VECODE;
+
+typedef enum {
 	LY_LLERR,
 	LY_LLWRN,
 	LY_LLVRB,
@@ -47,6 +53,7 @@ extern "Python" void lypy_log_cb(LY_LOG_LEVEL, const char *, const char *);
 void ly_set_log_clb(void (*)(LY_LOG_LEVEL, const char *, const char *), int);
 struct ly_err_item *ly_err_first(const struct ly_ctx *);
 void ly_err_clean(struct ly_ctx *, struct ly_err_item *);
+LY_VECODE ly_vecode(const struct ly_ctx *);
 
 struct lys_module {
 	const char *name;

commit 11da4bd8e7c9328cd575becf46901fb0d7aaf799

context: reset errors after LYVE_PATH_EXISTS
When calling create_data_path and the path already exists, we ignore the
error. However, since we are using LY_LOSTORE, all errors remain in the
context error cache until an actual error (that we don't ignore) occurs.

When that happens, all ignored LYVE_PATH_EXISTS errors are included in
the error message, polluting the actual error reason.

When ignoring LYVE_PATH_EXISTS, reset ly_errno and clear the context
error cache.

Fixes: 1b4f76b12109 ("data: add options to create_data_path")
Signed-off-by: Robin Jarry <robin@jarry.cc>

libyang/__init__.py
@@ -162,6 +162,8 @@ class Context:
         if lib.lypy_get_errno() != lib.LY_SUCCESS:
             if lib.ly_vecode(self._ctx) != lib.LYVE_PATH_EXISTS:
                 raise self.error('cannot create data path')
+            lib.ly_err_clean(self._ctx, ffi.NULL)
+            lib.lypy_set_errno(0)
         if not dnode and not force_return_value:
             return None
 
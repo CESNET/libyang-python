CESNET/libyang-python

commit 0a1e53c3c4818dfdefd76bc546a93c38eba0210e

data: optimize dict_to_dnode
Do not use lyd_new_path, it calls lyd_validate every time. Use simpler
functions (lyd_new, lyd_new_leaf) instead and use a similar algorithm as
the JSON parser (see parser_json.c). Use a cache for schema nodes
lookup.

Call lyd_validate() when parsing is done.

The unit tests were adapted accordingly.

Some timing benchmarks for the record: big dict with nested containers,
lists and various leaf types. YANG model itself is quite complex with
multiple augmentations, etc. Here is the time comparison to convert to
dict to dnode:

==================== ======== ======== ======== ======== ========
num. objects              500     1000     2000     5000    10000
==================== ======== ======== ======== ======== ========
before optimization     7.67s   31.18s  117.74s  758.51s       --
after optimization       .07s     .13s     .24s     .57s    1.16s
==================== ======== ======== ======== ======== ========

I did not manage to test with 10000 objects without the optimization, it
is completely unusable anyway. I had to interrupt the test after 20
minutes of processing before it was finished.

Signed-off-by: Robin Jarry <robin.jarry@6wind.com>
Acked-by: Samuel Gauthier <samuel.gauthier@6wind.com>
Acked-by: Olivier Matz <olivier.matz@6wind.com>

libyang/data.py
@@ -423,82 +423,132 @@ def dict_to_dnode(dic, module, parent=None, rpc=False, rpcreply=False):
 
     created = []
 
-    def _create(_schema, key, value=None):
-        nonlocal parent
-        dnode = _schema.context.create_data_path(
-            _schema.data_path() % key, parent=parent, value=value,
-            update=False, no_parent_ret=False,
-            force_return_value=False, rpc_output=rpcreply)
-        if dnode is not None:
-            created.append(dnode)
-            if parent is None:
-                parent = dnode
+    def _create_leaf(_parent, module, name, value, in_rpc_output=False):
+        if value is not None:
+            if isinstance(value, bool):
+                value = str(value).lower()
+            elif not isinstance(value, str):
+                value = str(value)
+        if in_rpc_output:
+            n = lib.lyd_new_output_leaf(
+                _parent, module._module, str2c(name), str2c(value))
+        else:
+            n = lib.lyd_new_leaf(
+                _parent, module._module, str2c(name), str2c(value))
+        if not n:
+            if _parent:
+                parent_path = repr(DNode.new(module.context, _parent).path())
+            else:
+                parent_path = 'module %r' % module.name()
+            raise module.context.error(
+                'failed to create leaf %r as a child of %s', name, parent_path)
+        created.append(n)
 
-    def _to_dnode(_dic, _schema, key=()):
-        name = _schema.name()
-        if name not in _dic:
-            name = _schema.fullname()
-            if name not in _dic:
-                return
-        data = _dic[name]
-        if isinstance(_schema, SContainer):
-            if not isinstance(data, dict):
-                raise TypeError('%s: python value is not a dict: %r'
-                                % (_schema.schema_path(), data))
-            _create(_schema, key)
-            for s in _schema:
-                _to_dnode(data, s, key)
-        elif isinstance(_schema, SRpc):
+    def _create_container(_parent, module, name, in_rpc_output=False):
+        if in_rpc_output:
+            n = lib.lyd_new_output(_parent, module._module, str2c(name))
+        else:
+            n = lib.lyd_new(_parent, module._module, str2c(name))
+        if not n:
+            if _parent:
+                parent_path = repr(DNode.new(module.context, _parent).path())
+            else:
+                parent_path = 'module %r' % module.name()
+            raise module.context.error(
+                'failed to create container/list/rpc %r as a child of %s',
+                name, parent_path)
+        created.append(n)
+        return n
+
+    schema_cache = {}
+
+    def _find_schema(schema_parent, name, prefix):
+        if isinstance(schema_parent, Module):
+            cache_key = (schema_parent._module, name, prefix)
+        else:
+            cache_key = (schema_parent._node, name, prefix)
+        snode, module = schema_cache.get(cache_key, (None, None))
+        if snode is not None:
+            return snode, module
+        if isinstance(schema_parent, SRpc):
             if rpc:
-                _schema = _schema.input()
+                schema_parent = schema_parent.input()
             elif rpcreply:
-                _schema = _schema.output()
+                schema_parent = schema_parent.output()
             else:
                 raise ValueError('rpc or rpcreply must be specified')
-            if not _schema:
+            if schema_parent is None:
                 # there may not be any input or any output node in the rpc
-                return
-            for s in _schema:
-                _to_dnode(data, s, key)
-        elif isinstance(_schema, SList):
-            if not isinstance(data, (list, tuple)):
-                raise TypeError('%s: python value is not a list/tuple: %r'
-                                % (_schema.schema_path(), data))
-            for element in data:
-                if not isinstance(element, dict):
-                    raise TypeError('%s: list element is not a dict: %r'
-                                    % (_schema.schema_path(), element))
-                try:
-                    next_key = []
-                    for k in _schema.keys():
-                        try:
-                            next_key.append(element[k.name()])
-                        except KeyError as _e:
-                            try:
-                                next_key.append(element[k.fullname()])
-                            except KeyError:
-                                raise _e
-                except KeyError as e:
-                    raise KeyError(
-                        "%s: key '%s' not present in list element: %r"
-                        % (_schema.schema_path(), e, element))
-                for s in _schema:
-                    _to_dnode(element, s, key + tuple(next_key))
-        elif isinstance(_schema, SLeafList):
-            if not isinstance(data, (list, tuple)):
-                raise TypeError('%s: python value is not a list/tuple: %r'
-                                % (_schema.schema_path(), data))
-            for element in data:
-                _create(_schema, key, element)
-        elif isinstance(_schema, SLeaf):
-            _create(_schema, key, data)
+                return None, None
+        for s in schema_parent:
+            if s.name() != name:
+                continue
+            mod = s.module()
+            if prefix is not None and mod.name() != prefix:
+                continue
+            snode = s
+            module = mod
+            break
+        schema_cache[cache_key] = (snode, module)
+        return snode, module
+
+    def _to_dnode(_dic, _schema, _parent=ffi.NULL, in_rpc_output=False):
+        for key, value in _dic.items():
+            if ':' in key:
+                prefix, name = name.split(':')
+            else:
+                prefix, name = None, key
+
+            s, module = _find_schema(_schema, name, prefix)
+            if not s:
+                continue
+
+            if isinstance(s, SLeaf):
+                _create_leaf(_parent, module, name, value, in_rpc_output)
+
+            elif isinstance(s, SLeafList):
+                if not isinstance(value, (list, tuple)):
+                    raise TypeError('%s: python value is not a list/tuple: %r'
+                                    % (s.schema_path(), value))
+                for v in value:
+                    _create_leaf(_parent, module, name, v, in_rpc_output)
+
+            elif isinstance(s, SRpc):
+                n = _create_container(_parent, module, name, in_rpc_output)
+                _to_dnode(value, s, n, rpcreply)
+
+            elif isinstance(s, SContainer):
+                n = _create_container(_parent, module, name, in_rpc_output)
+                _to_dnode(value, s, n, in_rpc_output)
+
+            elif isinstance(s, SList):
+                if not isinstance(value, (list, tuple)):
+                    raise TypeError('%s: python value is not a list/tuple: %r'
+                                    % (s.schema_path(), value))
+                for v in value:
+                    if not isinstance(v, dict):
+                        raise TypeError('%s: list element is not a dict: %r'
+                                        % (_schema.schema_path(), v))
+                    n = _create_container(_parent, module, name, in_rpc_output)
+                    _to_dnode(v, s, n, in_rpc_output)
+
+    result = None
 
     try:
-        for s in module:
-            _to_dnode(dic, s)
+        if parent is not None:
+            _parent = parent._node
+            _schema_parent = parent.schema()
+        else:
+            _parent = ffi.NULL
+            _schema_parent = module
+        _to_dnode(dic, _schema_parent, _parent,
+                  in_rpc_output=rpcreply and isinstance(parent, DRpc))
+        if created:
+            result = DNode.new(module.context, created[0])
+            result.validate(rpc=rpc, rpcreply=rpc)
     except:
         for c in reversed(created):
-            c.free(with_siblings=False)
+            lib.lyd_free(c)
         raise
 
-    return parent
+    return result

tests/test_data.py
@@ -8,6 +8,7 @@ from unittest.mock import patch
 
 from libyang import Context
 from libyang import LibyangError
+from libyang import lib
 from libyang.data import DContainer
 from libyang.data import DNode
 from libyang.data import DRpc
@@ -258,20 +259,35 @@ class DataTest(unittest.TestCase):
 
     def test_data_from_dict_invalid(self):
         module = self.ctx.get_module('yolo-system')
-        orig_create = Context.create_data_path
-        orig_free = DNode.free
+
         created = []
         freed = []
 
-        def wrapped_create(self, *args, **kwargs):
-            c = orig_create(self, *args, **kwargs)
-            if c is not None:
-                created.append(c)
-            return c
+        class FakeLib:
 
-        def wrapped_free(self, *args, **kwargs):
-            freed.append(self)
-            orig_free(self, *args, **kwargs)
+            def __init__(self, orig):
+                self.orig = orig
+
+            def lyd_new(self, *args):
+                c = self.orig.lyd_new(*args)
+                if c:
+                    created.append(c)
+                return c
+
+            def lyd_new_leaf(self, *args):
+                c = self.orig.lyd_new_leaf(*args)
+                if c:
+                    created.append(c)
+                return c
+
+            def lyd_free(self, dnode):
+                freed.append(dnode)
+                self.orig.lyd_free(dnode)
+
+            def __getattr__(self, name):
+                return getattr(self.orig, name)
+
+        fake_lib = FakeLib(lib)
 
         root = module.parse_data_dict({
             'conf': {
@@ -300,8 +316,7 @@ class DataTest(unittest.TestCase):
         }
 
         try:
-            with patch.object(Context, 'create_data_path', wrapped_create), \
-                    patch.object(DNode, 'free', wrapped_free):
+            with patch('libyang.data.lib', fake_lib):
                 with self.assertRaises(LibyangError):
                     root.merge_data_dict(invalid_dict)
             self.assertGreater(len(created), 0)

cffi/cdefs.h
@@ -433,6 +433,10 @@ struct lyd_node_leaf_list {
 
 struct ly_set *lyd_find_instance(const struct lyd_node *, const struct lys_node *);
 struct ly_set *lyd_find_path(const struct lyd_node *, const char *);
+struct lyd_node *lyd_new(struct lyd_node *, const struct lys_module *, const char *);
+struct lyd_node *lyd_new_leaf(struct lyd_node *, const struct lys_module *, const char *, const char *);
+struct lyd_node *lyd_new_output(struct lyd_node *, const struct lys_module *, const char *);
+struct lyd_node *lyd_new_output_leaf(struct lyd_node *, const struct lys_module *, const char *, const char *);
 struct lyd_node *lyd_new_path(struct lyd_node *, const struct ly_ctx *, const char *, void *, LYD_ANYDATA_VALUETYPE, int);
 struct lyd_node *lyd_first_sibling(struct lyd_node *);
 char *lyd_path(const struct lyd_node *);

commit 5d0bf0095eda98300d96b1f492ecbe1fd6a81301

dict_to_dnode: fix typo in validate flags
rpcreply=True was ignored and wrongfully set to True when rpc=True.

Fixes: 0a1e53c3c481 ("data: optimize dict_to_dnode")
Signed-off-by: Robin Jarry <robin@jarry.cc>

libyang/data.py
@@ -606,7 +606,7 @@ def dict_to_dnode(dic, module, parent=None, rpc=False, rpcreply=False,
                   in_rpc_output=rpcreply and isinstance(parent, DRpc))
         if created:
             result = DNode.new(module.context, created[0])
-            result.validate(rpc=rpc, rpcreply=rpc,
+            result.validate(rpc=rpc, rpcreply=rpcreply,
                             data=data, config=config, no_yanglib=no_yanglib)
     except:
         for c in reversed(created):

tests/test_data.py
@@ -345,7 +345,7 @@ class DataTest(unittest.TestCase):
         dnode = self.ctx.create_data_path('/yolo-system:format-disk')
         self.assertIsInstance(dnode, DRpc)
         dnode.merge_data_dict(
-            {'duration': 42}, rpcreply=True, strict=True, no_yanglib=True)
+            {'duration': 42}, rpcreply=True, strict=True)
         try:
             j = dnode.print_mem('json')
         finally:
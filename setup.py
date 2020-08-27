CESNET/libyang-python

commit 41e35729106c6638200103b9ac81fad6db19af36

setup: use git tags for python package version
Signed-off-by: Robin Jarry <robin@jarry.cc>

setup.py
@@ -19,10 +19,12 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+import datetime
 from distutils import log
 from distutils.command.build_clib import build_clib
 import multiprocessing
 import os
+import re
 import subprocess
 import sys
 
@@ -86,9 +88,81 @@ if os.environ.get('LIBYANG_INSTALL') != 'system':
     LIBRARIES.append(('yang', {'sources': ['clib']}))
 
 
+def _tag_to_pep440_version(tag):
+    git_tag_re = re.compile(r'''
+        ^
+        v(?P<major>\d+)\.
+        (?P<minor>\d+)\.
+        (?P<patch>\d+)
+        (\.post(?P<post>\d+))?
+        (-(?P<dev>\d+))?
+        (-g(?P<commit>.+))?
+        $
+        ''', re.VERBOSE)
+
+    match = git_tag_re.match(tag)
+    if match:
+        d = match.groupdict()
+        fmt = '{major}.{minor}.{patch}'
+        if d.get('post'):
+            fmt += '.post{post}'
+        if d.get('dev'):
+            d['patch'] = int(d['patch']) + 1
+            fmt += '.dev{dev}'
+        return fmt.format(**d)
+
+    return datetime.datetime.now().strftime('%Y.%m.%d')
+
+
+def _tag_from_git_describe():
+    if not os.path.isdir(os.path.join(HERE, '.git')):
+        raise ValueError('not in git repo')
+
+    out = subprocess.check_output(['git', 'describe', '--always'],
+                                  cwd=HERE, stderr=subprocess.STDOUT)
+    return out.strip().decode('utf-8')
+
+
+def _version_from_git_archive_id(git_archive_id='$Format:%ct %d$'):
+    if git_archive_id.startswith('$For''mat:'):
+        raise ValueError('not a git archive')
+
+    match = re.search(r'tag:\s*v([^,)]+)', git_archive_id)
+    if match:
+        # archived revision is tagged, use the tag
+        return _tag_to_pep440_version(match.group(1))
+
+    # archived revision is not tagged, use the commit date
+    tstamp = git_archive_id.strip().split()[0]
+    d = datetime.datetime.fromtimestamp(int(tstamp))
+
+
+def _version():
+    try:
+        tag = _tag_from_git_describe()
+        version = _tag_to_pep440_version(tag)
+        with open(os.path.join(HERE, 'VERSION'), 'w') as f:
+            f.write(version)
+        return version
+    except:
+        pass
+    try:
+        with open(os.path.join(HERE, 'VERSION'), 'r') as f:
+            version = f.read()
+        return version.strip()
+    except:
+        pass
+    try:
+        return _version_from_git_archive_id()
+    except:
+        pass
+
+    return 'latest'
+
+
 setuptools.setup(
     name='libyang',
-    version='0.16.78',
+    version=_version(),
     description='CFFI bindings to libyang',
     long_description=open('README.rst').read(),
     url='https://github.com/rjarry/libyang-cffi',

.gitignore
@@ -7,4 +7,5 @@ __pycache__/
 /dist/
 /*.egg-info/
 MANIFEST
+VERSION
 /.*

MANIFEST.in
@@ -8,6 +8,7 @@ include setup.py
 include setup.cfg
 include LICENSE
 include README.rst
+include VERSION
 global-exclude *.py[co]
 global-exclude *.so
 global-exclude *.o

commit ed353e25d30179a86346c62f0c3a289c6aacb0f6

setup: fix tag extraction from git archive
git_describe_to_pep440 expects a leading 'v'.

Fixes: 41e35729106c ("setup: use git tags for python package version")
Signed-off-by: Robin Jarry <robin.jarry@6wind.com>

setup.py
@@ -76,7 +76,7 @@ def get_version_from_archive_id(git_archive_id="$Format:%ct %d$"):
 
     # source was modified by git archive, try to parse the version from
     # the value of git_archive_id
-    match = re.search(r"tag:\s*v([^,)]+)", git_archive_id)
+    match = re.search(r"tag:\s*(v[^,)]+)", git_archive_id)
     if match:
         # archived revision is tagged, use the tag
         return git_describe_to_pep440(match.group(1))
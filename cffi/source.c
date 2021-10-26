bd0ccf792d1cee01f8cee66ea047fe2688a2f65a
Pepa Hajek <hajekpepa@gmail.com>
2021-10-26 15:00:24 +0200

    Remove all cffi related code
    In order to pass cffi build process againts libyang2. This allows to start with
    a "clean table" and build empty interface with cffi/build.py. Python library
    code is left untached.

--- cffi/source.c
+++ cffi/source.c
@@ -3,28 +3,3 @@
  * SPDX-License-Identifier: MIT
  */
 
-#include <libyang/libyang.h>
-
-#if (LY_VERSION_MAJOR != 1)
-#error "This version of libyang bindings only works with libyang 1.x"
-#endif
-#if (LY_VERSION_MINOR < 8)
-#error "Need at least libyang 1.8"
-#endif
-
-static LY_ERR lypy_get_errno(void)
-{
-	return ly_errno;
-}
-
-static void lypy_set_errno(LY_ERR err)
-{
-	ly_errno = err;
-}
-
-static uint8_t lypy_module_implemented(const struct lys_module *module)
-{
-	if (module)
-		return module->implemented;
-	return 0;
-}

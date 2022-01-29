cccb9d6b90d955a8a7c678a878b5eb2b5ff9a0c1
Wataru Ishida <wataru.ishid@gmail.com>
2022-01-29 06:57:20 +0000

    test: add keyless list test
    Keyless list is allowed for operational data. (see RFC7950 7.8.2.) It is
    used in ietf-netconf-notifications:netconf-config-change, for example.
    
    It is already supported, but untested, add a test case.
    
    Signed-off-by: Wataru Ishida <wataru.ishid@gmail.com>
    Signed-off-by: Samuel Gauthier <samuel.gauthier@6wind.com>

--- tests/test_schema.py
+++ tests/test_schema.py
@@ -56,7 +56,7 @@ class ModuleTest(unittest.TestCase):
 
     def test_mod_iter(self):
         children = list(iter(self.module))
-        self.assertEqual(len(children), 5)
+        self.assertEqual(len(children), 6)
 
     def test_mod_children_rpcs(self):
         rpcs = list(self.module.children(types=(SNode.RPC,)))

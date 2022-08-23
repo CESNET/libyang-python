CESNET/libyang-python

commit 806be4c3bc4fd83b7df142533be1238cf7a9e11d

Port to libyang 2
- There is no pretty print parameter for print functions (was LYP_FORMAT
  in 1.x). Also there were renamed functions freeing libyang structures
  from memory.

- Change test_data_parse_config_json() to be working

  cdefs.h: Add lyd_parse_mem() and its parser and validation options.

  context.py: Change keyword parameters for parse_data_mem() according
  to a parser parameters in libyang2.

  data.py: change parser flags, add validation flags. They have been
  change and split for parse_*() fuctions in libyang2.

  test_data.py: in test_data_parse_config_json(), change comparison data
  for assert. There is no "pretty" parameter in libyang2 anymore (its
  implicit now) and json data are printed with sorted keys.

  However, test_data_parse_config_json() still does not pass because
  resulting json have the "number" union listed with numbers as string
  not as integer. It looks like a bug in libyang2.

- Function print_mem() needs to be used with with_siblings parameter,
  otherwise prints only one unspecified node on the same level.

- Function feature_disable() was removed because there is no replace for
  lys_features_disable() in libyang2. The only way how to disable
  eparticular feature is to disable all and enable set required features
  again.

- Parsed and compiled node schema trees corresponds closely to each
  other. In order connecting both together LY_CTX_SET_PRIV_PARSED
  context option is set by default. It is necessary for some features
  (eg. node type resolving).

- Libyang 2 does not have derived types anymore. All the code refering
  derived types removed. Removed also "description()" method, there is
  no support for it anymore.

- Function data.print_dict() was rewritten. There is no function like
  lys_should_print() in libyang2 thus nodes converted to json cannot be
  limited by parameters of this function.

- Parsing edit operation from dictionary is not supported because

libyang/__init__.py
@@ -1,10 +1,22 @@
 # Copyright (c) 2018-2019 Robin Jarry
 # Copyright (c) 2020 6WIND S.A.
+# Copyright (c) 2021 RACOM s.r.o.
 # SPDX-License-Identifier: MIT
 
 
 from .context import Context
-from .data import DContainer, DDiff, DLeaf, DLeafList, DList, DNode, DNotif, DRpc
+from .data import (
+    DAnydata,
+    DAnyxml,
+    DContainer,
+    DDiff,
+    DLeaf,
+    DLeafList,
+    DList,
+    DNode,
+    DNotif,
+    DRpc,
+)
 from .diff import (
     BaseTypeAdded,
     BaseTypeRemoved,
@@ -73,7 +85,7 @@ from .schema import (
     SRpcInOut,
     Type,
 )
-from .util import LibyangError
+from .util import DataType, IOType, LibyangError
 from .xpath import (
     xpath_del,
     xpath_get,
@@ -122,6 +134,8 @@ __all__ = (
     "LengthAdded",
     "LengthRemoved",
     "LibyangError",
+    "IOType",
+    "DataType",
     "MandatoryAdded",
     "MandatoryRemoved",
     "Module",

.github/workflows/ci.yml
@@ -12,7 +12,7 @@ jobs:
       - uses: actions/checkout@v2
       - uses: actions/setup-python@v2
         with:
-          python-version: 3.8
+          python-version: 3.x
       - uses: actions/cache@v2
         with:
           path: ~/.cache/pip
@@ -27,18 +27,18 @@ jobs:
     strategy:
       matrix:
         include:
-          - python: 3.5
-            toxenv: py35
           - python: 3.6
             toxenv: py36
           - python: 3.7
             toxenv: py37
           - python: 3.8
             toxenv: py38
+          - python: 3.9
+            toxenv: py39
+          - python: "3.10"
+            toxenv: py310
           - python: pypy3
             toxenv: pypy3
-          - python: 3.x
-            toxenv: py3-libyang1
     steps:
       - uses: actions/checkout@v2
       - uses: actions/setup-python@v2

Makefile
@@ -7,7 +7,7 @@ lint:
 	tox -e lint
 
 tests:
-	tox -e py3-libyang1
+	tox -e py37
 
 format:
 	tox -e format

cffi/source.c
@@ -1,30 +1,14 @@
 /*
- * Copyright (c) 2018-2019 Robin Jarry
+ * Copyright (c) 2018-2022 Robin Jarry
  * SPDX-License-Identifier: MIT
  */
 
 #include <libyang/libyang.h>
+#include <libyang/version.h>
 
-#if (LY_VERSION_MAJOR != 1)
-#error "This version of libyang bindings only works with libyang 1.x"
+#if (LY_VERSION_MAJOR != 2)
+#error "This version of libyang bindings only works with libyang 2.x"
 #endif
-#if (LY_VERSION_MINOR < 8)
-#error "Need at least libyang 1.8"
+#if (LY_VERSION_MINOR < 19)
+#error "Need at least libyang 2.19"
 #endif
-
-static LY_ERR lypy_get_errno(void)
-{
-	return ly_errno;
-}
-
-static void lypy_set_errno(LY_ERR err)
-{
-	ly_errno = err;
-}
-
-static uint8_t lypy_module_implemented(const struct lys_module *module)
-{
-	if (module)
-		return module->implemented;
-	return 0;
-}

libyang/context.py
@@ -1,14 +1,22 @@
 # Copyright (c) 2018-2019 Robin Jarry
 # Copyright (c) 2020 6WIND S.A.
+# Copyright (c) 2021 RACOM s.r.o.
 # SPDX-License-Identifier: MIT
 
 import os
 from typing import IO, Any, Iterator, Optional, Union
 
 from _libyang import ffi, lib
-from .data import DNode, data_format, parser_flags, path_flags
+from .data import (
+    DNode,
+    data_format,
+    data_type,
+    parser_flags,
+    path_flags,
+    validation_flags,
+)
 from .schema import Module, SNode, schema_in_format
-from .util import LibyangError, c2str, deprecated, str2c
+from .util import DataType, IOType, LibyangError, c2str, data_load, str2c
 
 
 # -------------------------------------------------------------------------------------
@@ -20,12 +28,10 @@ class Context:
         self,
         search_path: Optional[str] = None,
         disable_searchdir_cwd: bool = True,
-        pointer=None,  # C type: "struct ly_ctx *"
+        yanglib_path: Optional[str] = None,
+        yanglib_fmt: str = "json",
         cdata=None,  # C type: "struct ly_ctx *"
     ):
-        if pointer is not None:
-            deprecated("pointer=", "cdata=", "2.0.0")
-            cdata = pointer
         if cdata is not None:
             self.cdata = ffi.cast("struct ly_ctx *", cdata)
             return  # already initialized
@@ -33,10 +39,29 @@ class Context:
         options = 0
         if disable_searchdir_cwd:
             options |= lib.LY_CTX_DISABLE_SEARCHDIR_CWD
+        # force priv parsed
+        options |= lib.LY_CTX_SET_PRIV_PARSED
+
+        self.cdata = None
+        ctx = ffi.new("struct ly_ctx **")
+
+        if yanglib_path is None:
+            if lib.ly_ctx_new(ffi.NULL, options, ctx) != lib.LY_SUCCESS:
+                raise self.error("cannot create context")
+        else:
+            if yanglib_fmt == "json":
+                fmt = lib.LYD_JSON
+            else:
+                fmt = lib.LYD_XML
+            ret = lib.ly_ctx_new_ylpath(
+                str2c(search_path), str2c(yanglib_path), fmt, options, ctx
+            )
+            if ret != lib.LY_SUCCESS:
+                raise self.error("cannot create context")
 
         self.cdata = ffi.gc(
-            lib.ly_ctx_new(ffi.NULL, options),
-            lambda ctx: lib.ly_ctx_destroy(ctx, ffi.NULL),
+            ctx[0],
+            lib.ly_ctx_destroy,
         )
         if not self.cdata:
             raise self.error("cannot create context")
@@ -51,16 +76,21 @@ class Context:
         if search_path:
             search_dirs.extend(search_path.strip(": \t\r\n'\"").split(":"))
 
+        if yanglib_path:
+            return
+
         for path in search_dirs:
             if not os.path.isdir(path):
                 continue
             if lib.ly_ctx_set_searchdir(self.cdata, str2c(path)) != 0:
                 raise self.error("cannot set search dir")
 
-    @property
-    def _ctx(self):
-        deprecated("_ctx", "cdata", "2.0.0")
-        return self.cdata
+    def get_yanglib_data(self, content_id_format=""):
+        dnode = ffi.new("struct lyd_node **")
+        ret = lib.ly_ctx_get_yanglib_data(self.cdata, dnode, str2c(content_id_format))
+        if ret != lib.LY_SUCCESS:
+            raise self.error("cannot get yanglib data")
+        return DNode.new(self, dnode[0])
 
     def destroy(self):
         if self.cdata is not None:
@@ -89,31 +119,47 @@ class Context:
 
         return LibyangError(msg)
 
-    def parse_module_file(self, fileobj: IO, fmt: str = "yang") -> Module:
-        if self.cdata is None:
-            raise RuntimeError("context already destroyed")
+    def parse_module(
+        self,
+        in_data: Union[IO, str],
+        in_type: IOType,
+        fmt: str = "yang",
+        features=None,
+    ):
+        data = ffi.new("struct ly_in **")
+        data_keepalive = []
+        ret = data_load(in_type, in_data, data, data_keepalive)
+        if ret != lib.LY_SUCCESS:
+            raise self.error("failed to read input data")
+
+        feat = ffi.NULL
+        if features:
+            feat = ffi.new(f"char *[{len(features) + 1}]")
+            features = [str2c(i) for i in features]
+            for i, val in enumerate(features):
+                feat[i] = val
+            feat[len(features)] = ffi.NULL
+
+        mod = ffi.new("struct lys_module **")
         fmt = schema_in_format(fmt)
-        mod = lib.lys_parse_fd(self.cdata, fileobj.fileno(), fmt)
-        if not mod:
-            raise self.error("cannot parse module")
+        if lib.lys_parse(self.cdata, data[0], fmt, feat, mod) != lib.LY_SUCCESS:
+            raise self.error("failed to parse module")
 
-        return Module(self, mod)
+        return Module(self, mod[0])
 
-    def parse_module_str(self, s: str, fmt: str = "yang") -> Module:
-        if self.cdata is None:
-            raise RuntimeError("context already destroyed")
-        fmt = schema_in_format(fmt)
-        mod = lib.lys_parse_mem(self.cdata, str2c(s), fmt)
-        if not mod:
-            raise self.error("cannot parse module")
+    def parse_module_file(
+        self, fileobj: IO, fmt: str = "yang", features=None
+    ) -> Module:
+        return self.parse_module(fileobj, IOType.FILE, fmt, features)
 
-        return Module(self, mod)
+    def parse_module_str(self, s: str, fmt: str = "yang", features=None) -> Module:
+        return self.parse_module(s, IOType.MEMORY, fmt, features)
 
     def load_module(self, name: str) -> Module:
         if self.cdata is None:
             raise RuntimeError("context already destroyed")
-        mod = lib.ly_ctx_load_module(self.cdata, str2c(name), ffi.NULL)
-        if not mod:
+        mod = lib.ly_ctx_load_module(self.cdata, str2c(name), ffi.NULL, ffi.NULL)
+        if mod == ffi.NULL:
             raise self.error("cannot load module")
 
         return Module(self, mod)
@@ -121,23 +167,51 @@ class Context:
     def get_module(self, name: str) -> Module:
         if self.cdata is None:
             raise RuntimeError("context already destroyed")
-        mod = lib.ly_ctx_get_module(self.cdata, str2c(name), ffi.NULL, False)
-        if not mod:
+        mod = lib.ly_ctx_get_module_latest(self.cdata, str2c(name))
+        if mod == ffi.NULL:
             raise self.error("cannot get module")
 
         return Module(self, mod)
 
-    def find_path(self, path: str) -> Iterator[SNode]:
+    def find_path(self, path: str, output: bool = False) -> Iterator[SNode]:
         if self.cdata is None:
             raise RuntimeError("context already destroyed")
-        node_set = lib.ly_ctx_find_path(self.cdata, str2c(path))
-        if not node_set:
+
+        flags = 0
+        if output:
+            flags |= lib.LYS_FIND_XP_OUTPUT
+
+        node_set = ffi.new("struct ly_set **")
+        if (
+            lib.lys_find_xpath(self.cdata, ffi.NULL, str2c(path), 0, node_set)
+            != lib.LY_SUCCESS
+        ):
+            raise self.error("cannot find path")
+
+        node_set = node_set[0]
+        if node_set.count == 0:
             raise self.error("cannot find path")
         try:
-            for i in range(node_set.number):
-                yield SNode.new(self, node_set.set.s[i])
+            for i in range(node_set.count):
+                yield SNode.new(self, node_set.snodes[i])
         finally:
-            lib.ly_set_free(node_set)
+            lib.ly_set_free(node_set, ffi.NULL)
+
+    def find_jsonpath(
+        self,
+        path: str,
+        root_node: Optional["libyang.SNode"] = None,
+        output: bool = False,
+    ) -> Optional["libyang.SNode"]:
+        if root_node is not None:
+            ctx_node = root_node.cdata
+        else:
+            ctx_node = ffi.NULL
+
+        ret = lib.lys_find_path(self.cdata, ctx_node, str2c(path), output)
+        if ret == ffi.NULL:
+            return None
+        return SNode.new(self, ret)
 
     def create_data_path(
         self,
@@ -151,7 +225,6 @@ class Context:
     ) -> Optional[DNode]:
         if self.cdata is None:
             raise RuntimeError("context already destroyed")
-        lib.lypy_set_errno(0)
         if value is not None:
             if isinstance(value, bool):
                 value = str(value).lower()
@@ -160,19 +233,20 @@ class Context:
         flags = path_flags(
             update=update, no_parent_ret=no_parent_ret, rpc_output=rpc_output
         )
-        dnode = lib.lyd_new_path(
+        dnode = ffi.new("struct lyd_node **")
+        ret = lib.lyd_new_path(
             parent.cdata if parent else ffi.NULL,
             self.cdata,
             str2c(path),
             str2c(value),
-            0,
             flags,
+            dnode,
         )
-        if lib.lypy_get_errno() != lib.LY_SUCCESS:
-            if lib.ly_vecode(self.cdata) != lib.LYVE_PATH_EXISTS:
+        dnode = dnode[0]
+        if ret != lib.LY_SUCCESS:
+            if lib.ly_vecode(self.cdata) != lib.LYVE_SUCCESS:
                 raise self.error("cannot create data path: %s", path)
             lib.ly_err_clean(self.cdata, ffi.NULL)
-            lib.lypy_set_errno(0)
         if not dnode and not force_return_value:
             return None
 
@@ -180,118 +254,186 @@ class Context:
             # This can happen when path points to an already created leaf and
             # its value does not change.
             # In that case, lookup the existing leaf and return it.
-            node_set = lib.lyd_find_path(parent.cdata, str2c(path))
+            node_set = ffi.new("struct ly_set **")
+            ret = lib.lyd_find_xpath(parent.cdata, str2c(path), node_set)
+            if ret != lib.LY_SUCCESS:
+                raise self.error("cannot find path: %s", path)
+
+            node_set = node_set[0]
             try:
-                if not node_set or not node_set.number:
+                if not node_set or not node_set.count:
                     raise self.error("cannot find path: %s", path)
-                dnode = node_set.set.s[0]
+                dnode = node_set.dnodes[0]
             finally:
-                lib.ly_set_free(node_set)
+                lib.ly_set_free(node_set, ffi.NULL)
 
         if not dnode:
             raise self.error("cannot find created path")
 
         return DNode.new(self, dnode)
 
-    def parse_data_mem(  # pylint: disable=too-many-arguments
+    def parse_op(
         self,
-        d: Union[str, bytes],
         fmt: str,
-        data: bool = False,
-        config: bool = False,
-        get: bool = False,
-        getconfig: bool = False,
-        edit: bool = False,
-        rpc: bool = False,
-        rpcreply: bool = False,
-        notification: bool = False,
-        strict: bool = False,
-        trusted: bool = False,
-        no_yanglib: bool = False,
-        rpc_request: Optional[DNode] = None,
-        data_tree: Optional[DNode] = None,
+        in_type: IOType,
+        in_data: Union[IO, str],
+        dtype: DataType,
+        parent: DNode = None,
     ) -> DNode:
+        fmt = data_format(fmt)
+        data = ffi.new("struct ly_in **")
+        data_keepalive = []
+        dtype = data_type(dtype)
+        ret = data_load(in_type, in_data, data, data_keepalive)
+        if ret != lib.LY_SUCCESS:
+            raise self.error("failed to read input data")
+
+        tree = ffi.new("struct lyd_node **", ffi.NULL)
+        op = ffi.new("struct lyd_node **", ffi.NULL)
+        par = ffi.new("struct lyd_node **", ffi.NULL)
+        if parent is not None:
+            par[0] = parent.cdata
+
+        ret = lib.lyd_parse_op(self.cdata, par[0], data[0], fmt, dtype, tree, op)
+        if ret != lib.LY_SUCCESS:
+            raise self.error("failed to parse input data")
+
+        return DNode.new(self, op[0])
+
+    def parse_op_mem(
+        self,
+        fmt: str,
+        data: str,
+        dtype: DataType = DataType.DATA_YANG,
+        parent: DNode = None,
+    ):
+        return self.parse_op(
+            fmt, in_type=IOType.MEMORY, in_data=data, dtype=dtype, parent=parent
+        )
+
+    def parse_data(
+        self,
+        fmt: str,
+        in_type: IOType,
+        in_data: Union[str, bytes, IO],
+        parent: DNode = None,
+        parser_lyb_mod_update: bool = False,
+        parser_no_state: bool = False,
+        parser_parse_only: bool = False,
+        parser_opaq: bool = False,
+        parser_ordered: bool = False,
+        parser_strict: bool = False,
+        validation_no_state: bool = False,
+        validation_validate_present: bool = False,
+    ) -> Optional[DNode]:
         if self.cdata is None:
             raise RuntimeError("context already destroyed")
-        flags = parser_flags(
-            data=data,
-            config=config,
-            get=get,
-            getconfig=getconfig,
-            edit=edit,
-            rpc=rpc,
-            rpcreply=rpcreply,
-            notification=notification,
-            strict=strict,
-            trusted=trusted,
-            no_yanglib=no_yanglib,
+        parser_flgs = parser_flags(
+            lyb_mod_update=parser_lyb_mod_update,
+            no_state=parser_no_state,
+            parse_only=parser_parse_only,
+            opaq=parser_opaq,
+            ordered=parser_ordered,
+            strict=parser_strict,
+        )
+        validation_flgs = validation_flags(
+            no_state=validation_no_state, validate_present=validation_validate_present
         )
         fmt = data_format(fmt)
+        encode = True
         if fmt == lib.LYD_LYB:
-            d = str2c(d, encode=False)
-        else:
-            d = str2c(d, encode=True)
-        args = []
-        if rpcreply:
-            if rpc_request is None:
-                raise ValueError("rpc_request node is required when rpcreply=True")
-            args.append(rpc_request.cdata)
-        if rpc or rpcreply or notification:
-            if data_tree is not None:
-                args.append(data_tree.cdata)
-            else:
-                args.append(ffi.cast("struct lyd_node *", ffi.NULL))
-        dnode = lib.lyd_parse_mem(self.cdata, d, fmt, flags, *args)
-        if not dnode:
+            encode = False
+        data = ffi.new("struct ly_in **")
+        data_keepalive = []
+        ret = data_load(in_type, in_data, data, data_keepalive, encode)
+        if ret != lib.LY_SUCCESS:
+            raise self.error("failed to read input data")
+
+        if parent is not None:
+            ret = lib.lyd_parse_data(
+                self.cdata,
+                parent.cdata,
+                data[0],
+                fmt,
+                parser_flgs,
+                validation_flgs,
+                ffi.NULL,
+            )
+            lib.ly_in_free(data[0], 0)
+            if ret != lib.LY_SUCCESS:
+                raise self.error("failed to parse data tree")
+            return None
+
+        dnode = ffi.new("struct lyd_node **")
+        ret = lib.lyd_parse_data(
+            self.cdata, ffi.NULL, data[0], fmt, parser_flgs, validation_flgs, dnode
+        )
+        lib.ly_in_free(data[0], 0)
+        if ret != lib.LY_SUCCESS:
             raise self.error("failed to parse data tree")
+
+        dnode = dnode[0]
+        if dnode == ffi.NULL:
+            return None
         return DNode.new(self, dnode)
+
+    def parse_data_mem(
+        self,
+        data: Union[str, bytes],
+        fmt: str,
+        parent: DNode = None,
+        parser_lyb_mod_update: bool = False,
+        parser_no_state: bool = False,
+        parser_parse_only: bool = False,
+        parser_opaq: bool = False,
+        parser_ordered: bool = False,
+        parser_strict: bool = False,
+        validation_no_state: bool = False,
+        validation_validate_present: bool = False,
+    ) -> Optional[DNode]:
+        return self.parse_data(
+            fmt,
+            in_type=IOType.MEMORY,
+            in_data=data,
+            parent=parent,
+            parser_lyb_mod_update=parser_lyb_mod_update,
+            parser_no_state=parser_no_state,
+            parser_parse_only=parser_parse_only,
+            parser_opaq=parser_opaq,
+            parser_ordered=parser_ordered,
+            parser_strict=parser_strict,
+            validation_no_state=validation_no_state,
+            validation_validate_present=validation_validate_present,
+        )
 
     def parse_data_file(
         self,
         fileobj: IO,
         fmt: str,
-        data: bool = False,
-        config: bool = False,
-        get: bool = False,
-        getconfig: bool = False,
-        edit: bool = False,
-        rpc: bool = False,
-        rpcreply: bool = False,
-        strict: bool = False,
-        trusted: bool = False,
-        no_yanglib: bool = False,
-        rpc_request: Optional[DNode] = None,
-        data_tree: Optional[DNode] = None,
-    ) -> DNode:
-        if self.cdata is None:
-            raise RuntimeError("context already destroyed")
-        flags = parser_flags(
-            data=data,
-            config=config,
-            get=get,
-            getconfig=getconfig,
-            edit=edit,
-            rpc=rpc,
-            rpcreply=rpcreply,
-            strict=strict,
-            trusted=trusted,
-            no_yanglib=no_yanglib,
+        parent: DNode = None,
+        parser_lyb_mod_update: bool = False,
+        parser_no_state: bool = False,
+        parser_parse_only: bool = False,
+        parser_opaq: bool = False,
+        parser_ordered: bool = False,
+        parser_strict: bool = False,
+        validation_no_state: bool = False,
+        validation_validate_present: bool = False,
+    ) -> Optional[DNode]:
+        return self.parse_data(
+            fmt,
+            in_type=IOType.FD,
+            in_data=fileobj,
+            parent=parent,
+            parser_lyb_mod_update=parser_lyb_mod_update,
+            parser_no_state=parser_no_state,
+            parser_parse_only=parser_parse_only,
+            parser_opaq=parser_opaq,
+            parser_ordered=parser_ordered,
+            parser_strict=parser_strict,
+            validation_no_state=validation_no_state,
+            validation_validate_present=validation_validate_present,
         )
-        fmt = data_format(fmt)
-        args = []
-        if rpcreply:
-            if rpc_request is None:
-                raise ValueError("rpc_request node is required when rpcreply=True")
-            args.append(rpc_request.cdata)
-        if rpc or rpcreply:
-            if data_tree is not None:
-                args.append(data_tree.cdata)
-            else:
-                args.append(ffi.cast("struct lyd_node *", ffi.NULL))
-        dnode = lib.lyd_parse_fd(self.cdata, fileobj.fileno(), fmt, flags, *args)
-        if not dnode:
-            raise self.error("failed to parse data tree")
-        return DNode.new(self, dnode)
 
     def __iter__(self) -> Iterator[Module]:
         """

libyang/keyed_list.py
@@ -140,7 +140,7 @@ class KeyedList(list):
         return k
 
     # unsupported list API methods
-    def __unsupported(self, *args, **kwargs):  # pylint: disable=unused-private-member
+    def __unsupported(self, *args, **kwargs):
         raise TypeError("unsupported operation for non-ordered lists")
 
     index = __unsupported

commit d73dcbef684567de76b275993854268804841275

data: remove DDiff
DDiff is not used since the port to libyang 2. Also fix the DNode.diff
return type, it is a DNode now, not a DDiff iterator.

Fixes: 806be4c3bc4f ("Port to libyang 2")
Signed-off-by: Samuel Gauthier <samuel.gauthier@6wind.com>

libyang/__init__.py
@@ -9,7 +9,6 @@ from .data import (
     DAnydata,
     DAnyxml,
     DContainer,
-    DDiff,
     DLeaf,
     DLeafList,
     DList,
@@ -106,7 +105,6 @@ __all__ = (
     "ConfigFalseRemoved",
     "Context",
     "DContainer",
-    "DDiff",
     "DLeaf",
     "DLeafList",
     "DList",

libyang/data.py
@@ -188,37 +188,6 @@ def diff_flags(with_defaults: bool = False) -> int:
 
 
 # -------------------------------------------------------------------------------------
-class DDiff:
-    """
-    Data tree diff
-    """
-
-    __slots__ = ("dtype", "first", "second")
-
-    def __init__(self, dtype, first: Optional["DNode"], second: Optional["DNode"]):
-        """
-        :arg dtype:
-            The type of the diff
-        :arg first:
-            The first DNode
-        :arg second:
-            The second DNode
-        """
-        self.dtype = dtype
-        self.first = first
-        self.second = second
-
-    def diff_type(self) -> str:
-        """Get diff type as string"""
-        return hex(self.dtype)
-
-    def __repr__(self) -> str:
-        return "<libyang.data.DDiff {} first={} second={}>".format(
-            self.diff_type(), self.first, self.second
-        )
-
-
-# -------------------------------------------------------------------------------------
 class DNode:
     """
     Data tree node.
@@ -496,7 +465,7 @@ class DNode:
         other: "DNode",
         no_siblings: bool = False,
         with_defaults: bool = False,
-    ) -> Iterator[DDiff]:
+    ) -> "DNode":
         flags = diff_flags(with_defaults=with_defaults)
         node_p = ffi.new("struct lyd_node **")
         if no_siblings:
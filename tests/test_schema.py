b62fb0d52498d9749918f1609b44b75b98c376c8
Stefan Gula <steweg@gmail.com>
2023-11-11 23:51:41 +0100

    schema: add fraction_digits support
    This patch introduces fraction_digits() and all_fraction_digits()
    functions for SLeaf and SLeafList.
    
    Fixes: #86
    Signed-off-by: Stefan Gula <steweg@gmail.com>
    Signed-off-by: Samuel Gauthier <samuel.gauthier@6wind.com>

--- tests/test_schema.py
+++ tests/test_schema.py
@@ -477,6 +477,14 @@ class LeafTypeTest(unittest.TestCase):
         leaf = next(self.ctx.find_path("/yolo-system:conf"))
         self.assertEqual(len(list(leaf.iter_tree(full=True))), 23)
 
+    def test_leaf_type_fraction_digits(self):
+        self.ctx.load_module("yolo-nodetypes")
+        leaf = next(self.ctx.find_path("/yolo-nodetypes:conf/percentage"))
+        self.assertIsInstance(leaf, SLeaf)
+        t = leaf.type()
+        self.assertIsInstance(t, Type)
+        self.assertEqual(next(t.all_fraction_digits(), None), 2)
+
 
 # -------------------------------------------------------------------------------------
 class LeafTest(unittest.TestCase):

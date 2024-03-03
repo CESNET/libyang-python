17cfe55d50b9544eeed310452f33daed69499c77
Stefan Gula <steweg@gmail.com>
2024-03-03 15:56:15 +0100

    context: add builtin_plugins_only options
    This patch adds builtin_plugins_only option, which allows user to use
    only basic YANG type plugins, instead of using advanced plugins as
    ipv4-address etc.
    
    Signed-off-by: Stefan Gula <steweg@gmail.com>

--- libyang/context.py
+++ libyang/context.py
@@ -31,6 +31,7 @@ class Context:
         explicit_compile: Optional[bool] = False,
         leafref_extended: bool = False,
         leafref_linking: bool = False,
+        builtin_plugins_only: bool = False,
         yanglib_path: Optional[str] = None,
         yanglib_fmt: str = "json",
         cdata=None,  # C type: "struct ly_ctx *"
@@ -50,6 +51,8 @@ class Context:
             options |= lib.LY_CTX_LEAFREF_EXTENDED
         if leafref_linking:
             options |= lib.LY_CTX_LEAFREF_LINKING
+        if builtin_plugins_only:
+            options |= lib.LY_CTX_BUILTIN_PLUGINS_ONLY
         # force priv parsed
         options |= lib.LY_CTX_SET_PRIV_PARSED
 

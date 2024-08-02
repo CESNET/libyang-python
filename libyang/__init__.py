CESNET/libyang-python

commit b7adea400520477f005a5b8c9857b9daf574de4b

schema: adds PNode and its exact variants
This patch introduces PNode alternative to SNode, which allows to
traverse parsed module tree including groupings, uses and other special
nodes.

Closes: https://github.com/CESNET/libyang-python/pull/102
Signed-off-by: Stefan Gula <steweg@gmail.com>
Signed-off-by: Samuel Gauthier <samuel.gauthier@6wind.com>

libyang/__init__.py
@@ -76,6 +76,23 @@ from .schema import (
     IfOrFeatures,
     Module,
     Must,
+    PAction,
+    PActionInOut,
+    PAnydata,
+    PAugment,
+    PCase,
+    PChoice,
+    PContainer,
+    PEnum,
+    PGrouping,
+    PLeaf,
+    PLeafList,
+    PList,
+    PNode,
+    PNotif,
+    PRefine,
+    PType,
+    PUses,
     Pattern,
     Revision,
     SAnydata,
@@ -152,6 +169,23 @@ __all__ = (
     "NodeTypeRemoved",
     "OrderedByUserAdded",
     "OrderedByUserRemoved",
+    "PAction",
+    "PActionInOut",
+    "PAnydata",
+    "PAugment",
+    "PCase",
+    "PChoice",
+    "PContainer",
+    "PEnum",
+    "PGrouping",
+    "PLeaf",
+    "PLeafList",
+    "PList",
+    "PNode",
+    "PNotif",
+    "PRefine",
+    "PType",
+    "PUses",
     "Pattern",
     "PatternAdded",
     "PatternRemoved",

cffi/cdefs.h
@@ -528,6 +528,25 @@ typedef enum {
 
 char* lysc_path(const struct lysc_node *, LYSC_PATH_TYPE, char *, size_t);
 
+struct lysp_when {
+    const char *cond;
+    ...;
+};
+
+struct lysp_refine {
+    const char *nodeid;
+    const char *dsc;
+    const char *ref;
+    struct lysp_qname *iffeatures;
+    struct lysp_restr *musts;
+    const char *presence;
+    struct lysp_qname *dflts;
+    uint32_t min;
+    uint32_t max;
+    struct lysp_ext_instance *exts;
+    uint16_t flags;
+};
+
 struct lysp_node_container {
     struct lysp_restr *musts;
     struct lysp_when *when;
@@ -615,12 +634,117 @@ struct lysp_node_list {
     ...;
 };
 
+struct lysp_node_choice {
+    struct lysp_node *child;
+    struct lysp_when *when;
+    struct lysp_qname dflt;
+    ...;
+};
+
+struct lysp_node_case {
+    struct lysp_node *child;
+    struct lysp_when *when;
+    ...;
+};
+
+struct lysp_node_anydata {
+    struct lysp_restr *musts;
+    struct lysp_when *when;
+    ...;
+};
+
+struct lysp_node_uses {
+    struct lysp_refine *refines;
+    struct lysp_node_augment *augments;
+    struct lysp_when *when;
+    ...;
+};
+
+struct lysp_node_action_inout {
+    struct lysp_restr *musts;
+    struct lysp_tpdf *typedefs;
+    struct lysp_node_grp *groupings;
+    struct lysp_node *child;
+    ...;
+};
+
+struct lysp_node_action {
+    union {
+        struct lysp_node node;
+        struct {
+            struct lysp_node_action *next;
+            ...;
+        };
+    };
+    struct lysp_tpdf *typedefs;
+    struct lysp_node_grp *groupings;
+    struct lysp_node_action_inout input;
+    struct lysp_node_action_inout output;
+    ...;
+};
+
+struct lysp_node_notif {
+    union {
+        struct lysp_node node;
+        struct {
+            struct lysp_node_notif *next;
+            ...;
+        };
+    };
+    struct lysp_restr *musts;
+    struct lysp_tpdf *typedefs;
+    struct lysp_node_grp *groupings;
+    struct lysp_node *child;
+    ...;
+};
+
+struct lysp_node_grp {
+    union {
+        struct lysp_node node;
+        struct {
+            struct lysp_node_grp *next;
+            ...;
+        };
+    };
+    struct lysp_tpdf *typedefs;
+    struct lysp_node_grp *groupings;
+    struct lysp_node *child;
+    struct lysp_node_action *actions;
+    struct lysp_node_notif *notifs;
+    ...;
+};
+
+struct lysp_node_augment {
+    union {
+        struct lysp_node node;
+        struct {
+            struct lysp_node_augment *next;
+            ...;
+        };
+    };
+    struct lysp_node *child;
+    struct lysp_when *when;
+    struct lysp_node_action *actions;
+    struct lysp_node_notif *notifs;
+    ...;
+};
+
 struct lysc_type {
     const char *name;
     struct lysc_ext_instance *exts;
     struct lyplg_type *plugin;
     LY_DATA_TYPE basetype;
     uint32_t refcount;
+};
+
+struct lysp_type_enum {
+    const char *name;
+    const char *dsc;
+    const char *ref;
+    int64_t value;
+    struct lysp_qname *iffeatures;
+    struct lysp_ext_instance *exts;
+    uint16_t flags;
 };
 
 struct lysp_type {

tests/test_schema.py
@@ -13,8 +13,25 @@ from libyang import (
     LibyangError,
     Module,
     Must,
+    PAction,
+    PActionInOut,
+    PAnydata,
     Pattern,
+    PAugment,
+    PCase,
+    PChoice,
+    PContainer,
+    PGrouping,
+    PLeaf,
+    PLeafList,
+    PList,
+    PNode,
+    PNotif,
+    PRefine,
+    PType,
+    PUses,
     Revision,
+    SAnydata,
     SCase,
     SChoice,
     SContainer,
@@ -287,6 +304,79 @@ class ContainerTest(unittest.TestCase):
         tree = list(self.container.iter_tree(full=True))
         self.assertEqual(len(tree), 25)
 
+    def test_container_parsed(self):
+        pnode = self.container.parsed()
+        self.assertIsInstance(pnode, PContainer)
+        self.assertIsNone(next(pnode.musts(), None))
+        self.assertIsNone(pnode.when_condition())
+        self.assertIsNone(pnode.presence())
+        self.assertIsNone(next(pnode.typedefs(), None))
+        self.assertIsNone(next(pnode.groupings(), None))
+        self.assertIsNotNone(next(iter(pnode)))
+        self.assertIsNone(next(pnode.actions(), None))
+        self.assertIsNone(next(pnode.notifications(), None))
+
+
+# -------------------------------------------------------------------------------------
+class UsesTest(unittest.TestCase):
+    def setUp(self):
+        self.ctx = Context(YANG_DIR)
+        mod = self.ctx.load_module("yolo-nodetypes")
+        mod.feature_enable_all()
+
+    def tearDown(self):
+        self.ctx.destroy()
+        self.ctx = None
+
+    def test_uses_parsed(self):
+        snode = next(self.ctx.find_path("/yolo-nodetypes:cont2"))
+        self.assertIsInstance(snode, SContainer)
+        pnode = snode.parsed()
+        self.assertIsInstance(pnode, PContainer)
+        pnode = next(iter(pnode))
+        self.assertIsInstance(pnode, PUses)
+
+        ref_pnode = next(pnode.refines())
+        self.assertIsInstance(ref_pnode, PRefine)
+        self.assertEqual("cont3/leaf1", ref_pnode.nodeid())
+        self.assertIsNone(ref_pnode.description())
+        self.assertIsNone(ref_pnode.reference())
+        self.assertIsNone(next(ref_pnode.if_features(), None))
+        self.assertIsNone(next(ref_pnode.musts(), None))
+        self.assertIsNone(ref_pnode.presence())
+        self.assertIsNone(next(ref_pnode.defaults(), None))
+        self.assertEqual(0, ref_pnode.min_elements())
+        self.assertIsNone(ref_pnode.max_elements())
+        self.assertIsNone(next(ref_pnode.extensions(), None))
+
+        aug_pnode = next(pnode.augments())
+        self.assertIsInstance(aug_pnode, PAugment)
+        self.assertIsNotNone(next(iter(aug_pnode)))
+        self.assertIsNone(aug_pnode.when_condition())
+        self.assertIsNone(next(aug_pnode.actions(), None))
+        self.assertIsNone(next(aug_pnode.notifications(), None))
+
+
+# -------------------------------------------------------------------------------------
+class GroupingTest(unittest.TestCase):
+    def setUp(self):
+        self.ctx = Context(YANG_DIR)
+
+    def tearDown(self):
+        self.ctx.destroy()
+        self.ctx = None
+
+    def test_grouping_parsed(self):
+        mod = self.ctx.load_module("yolo-nodetypes")
+        pnode = next(mod.groupings())
+        self.assertIsInstance(pnode, PGrouping)
+        self.assertIsNone(next(pnode.typedefs(), None))
+        self.assertIsNone(next(pnode.groupings(), None))
+        child = next(iter(pnode))
+        self.assertIsNotNone(child)
+        self.assertIsNone(next(pnode.actions(), None))
+        self.assertIsNone(next(pnode.notifications(), None))
+
 
 # -------------------------------------------------------------------------------------
 class ListTest(unittest.TestCase):
@@ -363,6 +453,25 @@ class ListTest(unittest.TestCase):
         self.assertEqual(list2.min_elements(), 0)
         self.assertEqual(list2.max_elements(), None)
 
+    def test_list_parsed(self):
+        list1 = next(self.ctx.find_path("/yolo-nodetypes:conf/list1"))
+        self.assertIsInstance(list1, SList)
+        pnode = list1.parsed()
+        self.assertIsInstance(pnode, PList)
+        self.assertIsNone(next(pnode.musts(), None))
+        self.assertIsNone(pnode.when_condition())
+        self.assertEqual("leaf1", pnode.key())
+        self.assertIsNone(next(pnode.typedefs(), None))
+        self.assertIsNone(next(pnode.groupings(), None))
+        child = next(iter(pnode))
+        self.assertIsInstance(child, PLeaf)
+        self.assertIsNone(next(pnode.actions(), None))
+        self.assertIsNone(next(pnode.notifications(), None))
+        self.assertEqual("leaf2 leaf3", next(pnode.uniques()))
+        self.assertEqual(2, pnode.min_elements())
+        self.assertEqual(10, pnode.max_elements())
+        self.assertFalse(pnode.ordered())
+
 
 # -------------------------------------------------------------------------------------
 class RpcTest(unittest.TestCase):
@@ -397,6 +506,21 @@ class RpcTest(unittest.TestCase):
 
     def test_rpc_no_parent(self):
         self.assertIsNone(self.rpc.parent())
+
+    def test_rpc_parsed(self):
+        self.assertIsInstance(self.rpc, SRpc)
+        pnode = self.rpc.parsed()
+        self.assertIsInstance(pnode, PAction)
+        self.assertIsNone(next(pnode.typedefs(), None))
+        self.assertIsNone(next(pnode.groupings(), None))
+        pnode2 = pnode.input()
+        self.assertIsInstance(pnode2, PActionInOut)
+        self.assertIsInstance(pnode.output(), PActionInOut)
+        self.assertIsNone(next(pnode2.musts(), None))
+        self.assertIsNone(next(pnode2.typedefs(), None))
+        self.assertIsNone(next(pnode2.groupings(), None))
+        pnode3 = next(iter(pnode2))
+        self.assertIsInstance(pnode3, PLeaf)
 
 
 # -------------------------------------------------------------------------------------
@@ -536,6 +660,28 @@ class LeafTypeTest(unittest.TestCase):
         self.assertIsInstance(t, Type)
         self.assertFalse(t.require_instance())
 
+    def test_leaf_type_parsed(self):
+        leaf = next(self.ctx.find_path("/yolo-system:conf/yolo-system:hostname"))
+        self.assertIsInstance(leaf, SLeaf)
+        t = leaf.type()
+        self.assertIsInstance(t, Type)
+        pnode = t.parsed()
+        self.assertIsInstance(pnode, PType)
+        self.assertEqual("types:host", pnode.name())
+        self.assertIsNone(pnode.range())
+        self.assertIsNone(pnode.length())
+        self.assertIsNone(next(pnode.patterns(), None))
+        self.assertIsNone(next(pnode.enums(), None))
+        self.assertIsNone(next(pnode.bits(), None))
+        self.assertIsNone(pnode.path())
+        self.assertIsNone(next(pnode.bases(), None))
+        self.assertIsNone(next(pnode.types(), None))
+        self.assertIsNone(next(pnode.extensions(), None))
+        self.assertIsNotNone(pnode.pmod())
+        self.assertIsNone(pnode.compiled())
+        self.assertEqual(0, pnode.fraction_digits())
+        self.assertFalse(pnode.require_instance())
+
 
 # -------------------------------------------------------------------------------------
 class LeafTest(unittest.TestCase):
@@ -559,6 +705,41 @@ class LeafTest(unittest.TestCase):
     def test_leaf_default(self):
         leaf = next(self.ctx.find_path("/yolo-nodetypes:conf/percentage"))
         self.assertIsInstance(leaf.default(), float)
+
+    def test_leaf_parsed(self):
+        leaf = next(self.ctx.find_path("/yolo-nodetypes:conf/percentage"))
+        self.assertIsInstance(leaf, SLeaf)
+        pnode = leaf.parsed()
+        self.assertIsInstance(pnode, PLeaf)
+        must = next(pnode.musts())
+        self.assertIsInstance(must, Must)
+        self.assertEqual(must.error_message(), "ERROR1")
+        must = next(leaf.must_conditions())
+        self.assertIsInstance(must, str)
+        self.assertIsNone(pnode.when_condition())
+        self.assertIsInstance(pnode.type(), PType)
+        self.assertIsNone(pnode.units())
+        self.assertEqual("10.2", pnode.default())
+        self.assertFalse(pnode.is_key())
+
+        # test basic PNode settings
+        self.assertIsNotNone(pnode.parent())
+        self.assertEqual(PNode.LEAF, pnode.nodetype())
+        self.assertIsNotNone(next(pnode.siblings()))
+        self.assertEqual("<libyang.schema.PLeaf: percentage>", repr(pnode))
+        self.assertIsNone(pnode.description())
+        self.assertIsNone(pnode.reference())
+        self.assertIsNone(next(pnode.if_features(), None))
+        self.assertIsNone(next(pnode.extensions(), None))
+        self.assertIsNone(pnode.get_extension("test", prefix="test"))
+        self.assertFalse(pnode.config_set())
+        self.assertFalse(pnode.config_false())
+        self.assertFalse(pnode.mandatory())
+        self.assertFalse(pnode.deprecated())
+        self.assertFalse(pnode.obsolete())
+        self.assertEqual("current", pnode.status())
+
+    NODETYPE_CLASS = {}
 
 
 # -------------------------------------------------------------------------------------
@@ -587,6 +768,20 @@ class LeafListTest(unittest.TestCase):
         self.assertEqual(leaflist2.min_elements(), 0)
         self.assertEqual(leaflist2.max_elements(), None)
 
+    def test_leaf_list_parsed(self):
+        leaflist = next(self.ctx.find_path("/yolo-nodetypes:conf/ratios"))
+        self.assertIsInstance(leaflist, SLeafList)
+        pnode = leaflist.parsed()
+        self.assertIsInstance(pnode, PLeafList)
+        self.assertIsNone(next(pnode.musts(), None))
+        self.assertIsNone(pnode.when_condition())
+        self.assertIsInstance(pnode.type(), PType)
+        self.assertIsNone(pnode.units())
+        self.assertEqual("2.5", next(pnode.defaults()))
+        self.assertEqual(0, pnode.min_elements())
+        self.assertIsNone(pnode.max_elements())
+        self.assertFalse(pnode.ordered())
+
 
 # -------------------------------------------------------------------------------------
 class ChoiceTest(unittest.TestCase):
@@ -603,3 +798,60 @@ class ChoiceTest(unittest.TestCase):
         choice = next(conf.children((SNode.CHOICE,), with_choice=True))
         self.assertIsInstance(choice, SChoice)
         self.assertIsInstance(choice.default(), SCase)
+
+    def test_choice_parsed(self):
+        conf = next(self.ctx.find_path("/yolo-system:conf"))
+        choice = next(conf.children((SNode.CHOICE,), with_choice=True))
+        self.assertIsInstance(choice, SChoice)
+        pnode = choice.parsed()
+        self.assertIsInstance(pnode, PChoice)
+
+        case_pnode = next(iter(pnode))
+        self.assertIsInstance(case_pnode, PCase)
+        self.assertIsNotNone(next(iter(case_pnode)))
+        self.assertIsNone(case_pnode.when_condition())
+
+        self.assertIsNone(pnode.when_condition())
+        self.assertEqual("red", pnode.default())
+
+
+# -------------------------------------------------------------------------------------
+class AnydataTest(unittest.TestCase):
+    def setUp(self):
+        self.ctx = Context(YANG_DIR)
+        self.ctx.load_module("yolo-nodetypes")
+
+    def tearDown(self):
+        self.ctx.destroy()
+        self.ctx = None
+
+    def test_anydata_parsed(self):
+        snode = next(self.ctx.find_path("/yolo-nodetypes:any1"))
+        self.assertIsInstance(snode, SAnydata)
+        pnode = snode.parsed()
+        self.assertIsInstance(pnode, PAnydata)
+        self.assertIsNone(next(pnode.musts(), None))
+        self.assertEqual("../cont2", pnode.when_condition())
+
+
+# -------------------------------------------------------------------------------------
+class NotificationTest(unittest.TestCase):
+    def setUp(self):
+        self.ctx = Context(YANG_DIR)
+        self.ctx.load_module("yolo-nodetypes")
+
+    def tearDown(self):
+        self.ctx.destroy()
+        self.ctx = None
+
+    def test_notification_parsed(self):
+        snode = next(self.ctx.find_path("/yolo-nodetypes:cont2"))
+        self.assertIsInstance(snode, SContainer)
+        pnode = snode.parsed()
+        self.assertIsInstance(pnode, PContainer)
+        pnode = next(pnode.notifications())
+        self.assertIsInstance(pnode, PNotif)
+        self.assertIsNone(next(pnode.musts(), None))
+        self.assertIsNone(next(pnode.typedefs(), None))
+        self.assertIsNone(next(pnode.groupings(), None))
+        self.assertIsNotNone(next(iter(pnode)))

tests/yang/yolo/yolo-nodetypes.yang
@@ -81,4 +81,35 @@ module yolo-nodetypes {
   leaf test1 {
     type uint8;
   }
+
+  grouping grp1 {
+    container cont3 {
+      leaf leaf1 {
+        type string;
+      }
+    }
+  }
+
+  container cont2 {
+    presence "special container enabled";
+    uses grp1 {
+      refine cont3/leaf1 {
+        mandatory true;
+      }
+      augment cont3 {
+        leaf leaf2 {
+          type int8;
+        }
+      }
+    }
+    notification interface-enabled {
+      leaf by-user {
+        type string;
+      }
+    }
+  }
+
+  anydata any1 {
+    when "../cont2";
+  }
 }

commit a3132f808bb0e78bc5255dde8acf68c2c549316d

libyang: fix import order
I prefer as it was, but isort doesn't, let's make him happy.

Fixes: b7adea400520 ("schema: adds PNode and its exact variants")
Signed-off-by: Samuel Gauthier <samuel.gauthier@6wind.com>

libyang/__init__.py
@@ -79,6 +79,7 @@ from .schema import (
     PAction,
     PActionInOut,
     PAnydata,
+    Pattern,
     PAugment,
     PCase,
     PChoice,
@@ -93,7 +94,6 @@ from .schema import (
     PRefine,
     PType,
     PUses,
-    Pattern,
     Revision,
     SAnydata,
     SAnyxml,